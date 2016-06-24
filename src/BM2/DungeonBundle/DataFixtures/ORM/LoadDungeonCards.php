b05669168915067b0a790b33b6898fd40c49687a
Zanaras <Andrew@SeventhGamers.com>
2016-06-24 03:37:55 -0500

    Update LoadDungeonCards.php
    Adding Slay Slime card.

--- src/BM2/DungeonBundle/DataFixtures/ORM/LoadDungeonCards.php
+++ src/BM2/DungeonBundle/DataFixtures/ORM/LoadDungeonCards.php
@@ -16,27 +16,29 @@ class LoadDungeonCards extends AbstractFixture implements OrderedFixtureInterfac
 		'basic.leave'		=> array('rarity' =>    0, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>false)),
 		'basic.rest'		=> array('rarity' =>  500, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>false)),
 		'basic.scout'		=> array('rarity' =>  750, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>false)),
-		'basic.plunder'	=> array('rarity' =>  800,	'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>true, 'dungeoneer'=>false)),
+		'basic.plunder'		=> array('rarity' =>  800, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>true, 'dungeoneer'=>false)),
 		'basic.fight'		=> array('rarity' => 1000, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
+
 
 		// common / usual cards
 		'action.wait'		=> array('rarity' => 1000, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>false)),
 		'action.untrap1'	=> array('rarity' =>  600, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>true, 'dungeoneer'=>false)),
 		'fight.double'		=> array('rarity' =>  500, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
 		'fight.strong'		=> array('rarity' =>  500, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
-		'fight.hit'			=> array('rarity' =>  500, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
+		'fight.hit'		=> array('rarity' =>  500, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
 		'fight.weak'		=> array('rarity' =>  650, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
+		'fight.slime'		=> array('rarity' =>  550, 'monsterclass'=> 'slime', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false))
 
 
 		// uncommon cards
-		'fight.stealth'	=> array('rarity' =>  250, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
+		'fight.stealth'		=> array('rarity' =>  250, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
 		'fight.sweep'		=> array('rarity' =>  400, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
 		'fight.swing'		=> array('rarity' =>  350, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
 		'fight.bomb1'		=> array('rarity' =>  250, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
 		'scout.double'		=> array('rarity' =>  300, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>false)),
 		'action.heal1'		=> array('rarity' =>  400, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>true)),
 		'action.heal2'		=> array('rarity' =>  200, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>true)),
-		'action.distracta'=> array('rarity' =>  400, 'monsterclass'=> 'animal', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
+		'action.distracta'	=> array('rarity' =>  400, 'monsterclass'=> 'animal', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
 		'action.rest2'		=> array('rarity' =>  200, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>false)),
 		'action.untrap2'	=> array('rarity' =>  300, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>true, 'dungeoneer'=>false)),
 
@@ -45,10 +47,10 @@ class LoadDungeonCards extends AbstractFixture implements OrderedFixtureInterfac
 		'action.heal3'		=> array('rarity' =>  100, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>true)),
 		'action.rest3'		=> array('rarity' =>   75, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>false)),
 		'action.untrap3'	=> array('rarity' =>  100, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>true, 'dungeoneer'=>false)),
-		'scout.tripple'	=> array('rarity' =>   50, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>false)),
+		'scout.tripple'		=> array('rarity' =>   50, 'monsterclass'=> '', 'target'=>array('monster'=>false, 'treasure'=>false, 'dungeoneer'=>false)),
 		'fight.kill'		=> array('rarity' =>  100, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
 		'fight.sure'		=> array('rarity' =>  100, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
-		'fight.tripple'	=> array('rarity' =>  100, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
+		'fight.tripple'		=> array('rarity' =>  100, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
 		'fight.bomb2'		=> array('rarity' =>   75, 'monsterclass'=> '', 'target'=>array('monster'=>true, 'treasure'=>false, 'dungeoneer'=>false)),
 
 	);
