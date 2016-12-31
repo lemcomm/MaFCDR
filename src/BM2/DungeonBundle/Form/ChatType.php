72803c94260bd8d72a4183d1e03492b6a61b6773
Andrew <Andrew@SeventhGamers.com>
2017-01-01 08:01:46 +0900

    Update ChatType.php
    Making Symfony2.7 compliant and fixing existing error.

--- src/BM2/DungeonBundle/Form/ChatType.php
+++ src/BM2/DungeonBundle/Form/ChatType.php
@@ -4,7 +4,7 @@ namespace BM2\DungeonBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bundle\FrameworkBundle\Translation\Translator;
 
 
@@ -14,7 +14,7 @@ class ChatType extends AbstractType {
 		return 'chat';
 	}
 
-	public function configureOptions(OptionsResolverInterface $resolver) {
+	public function configureOptions(OptionsResolver $resolver) {
 		$resolver->setDefaults(array(
 			'intention'       	=> 'chat_14',
 			'data_class'			=> 'BM2\DungeonBundle\Entity\DungeonMessage',
