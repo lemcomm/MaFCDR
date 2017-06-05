846b15f08dca37084f76830665670db60cddb09f
Andrew <Zanaras@users.noreply.github.com>
2017-06-05 18:41:45 +0900

    Update quarterhourly.sh

--- quarterhourly.sh
+++ quarterhourly.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-LOGDIR="/home/maf/logs"
-APP="/home/maf/symfony/app/console"
+LOGDIR="/var/www/maf/app/logs"
+APP="/var/www/maf/app/console"
 
 #LOGDIR="/tmp"
 #APP="./app/console"
 
 # Fix the fucking permissions that never stick:
-sudo setfacl -dR -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
-sudo setfacl -R -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
+# sudo setfacl -dR -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
+# sudo setfacl -R -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
 
 
 php $APP --env=prod maf:process:battles -t 5 2>&1 >> $LOGDIR/quarterhourly.log
