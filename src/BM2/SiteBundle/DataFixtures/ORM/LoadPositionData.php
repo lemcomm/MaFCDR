c18607f1ab968108a3a71b1afde450d8c936b819
Andrew <Zanaras@users.noreply.github.com>
2017-06-08 10:31:35 +0900

    Create LoadPositionData.php

---
+++ src/BM2/SiteBundle/DataFixtures/ORM/LoadPositionData.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BM2\SiteBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use BM2\SiteBundle\Entity\PositionType;
+
+class LoadPositionData extends AbstractFixture implements OrderedFixtureInterface {
+
+	private $positiontypes = array(
+		'advisory'		=> array('hidden' => false),
+		'executive'		=> array('hidden' => false),
+		'family'		=> array('hidden' => false),
+		'foreign affairs'	=> array('hidden' => false),
+		'history'		=> array('hidden' => false),
+		'intelligence'		=> array('hidden' => false),
+		'interior'		=> array('hidden' => false),
+		'judicial'		=> array('hidden' => false),
+		'legislative'		=> array('hidden' => false),
+		'military'		=> array('hidden' => false),
+		'revenue'		=> array('hidden' => false),
+		'other'			=> array('hidden' => false)
+	);
+	
+	/**
+	 * {@inheritDoc}
+	 */	 
+	public function getOrder() {
+		return 1; // or anywhere, really
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function load(ObjectManager $manager) {
+		foreach ($this->positiontypes as $name=>$data) {
+			$type = new PositionType;
+			$type->setName($name);
+			$type->setHidden($data['hidden']);
+			$manager->persist($type);
+		}
+		$manager->flush();
+	}
+}
