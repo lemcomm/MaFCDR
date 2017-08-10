be9caba19ef3557d4c8513b5ec7b4decd66ae1b6
Andrew <Zanaras@users.noreply.github.com>
2017-08-10 20:30:09 +0900

    Update RealmPosition.php

--- src/BM2/SiteBundle/Entity/RealmPosition.php
+++ src/BM2/SiteBundle/Entity/RealmPosition.php
@@ -5,4 +5,10 @@ namespace BM2\SiteBundle\Entity;
 
 class RealmPosition {
   
+        public function getPosType(){
+                if($this->type() == NULL) {
+                        return 'other';
+                } else return $this->type();
+        }
+        
 }
