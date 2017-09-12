5314f68196b27fea99435a7feaaf7466416862ed
Andrew <Zanaras@users.noreply.github.com>
2017-09-12 20:17:05 +0900

    Create Request.php
    Blatantly copying the actions table, because in all honesty, this is going to function VERY similarly, in that it will involve a bunch of columns for different things, involve atleast one text string, and a metric TON of different IDs down the line.
    
    Thankfully, it'll never be quite as big as the actions table, because these will all be one-offs, and I'll probably build in expiration.

---
+++ src/BM2/SiteBundle/Entity/Request.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace BM2\SiteBundle\Entity;
+
+class Request {
+
+	public function __toString() {
+		return "action {$this->id} - {$this->type}";
+	}
+
+
+	public function onPreRemove() {
+		// this doesn't work with cascade
+		if ($this->character) {
+			$this->character->removeAction($this);
+		}
+		if ($this->target_settlement) {
+			$this->target_settlement->removeRelatedAction($this);
+		}
+		if ($this->target_battlegroup) {
+			$this->target_battlegroup->removeRelatedAction($this);
+		}
+	}
+}
