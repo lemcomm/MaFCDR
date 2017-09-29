411e8f7ecaec33f945f84def8431f0af5c7e0fdb
Andrew <Zanaras@users.noreply.github.com>
2017-09-29 16:27:01 -0500

    Update Exhibit.php

--- src/BM2/SiteBundle/Entity/Exhibit.php
+++ src/BM2/SiteBundle/Entity/Exhibit.php
@@ -4,36 +4,4 @@ namespace BM2\SiteBundle\Entity;
 
 class Exhibit {
 
-
-	public function startExhibitConstruction($workers) {
-		$this->setActive(false);
-		$this->setWorkers($workers);
-		$this->setCondition(-$this->getType()->getBuildHours()); // negative value - if we reach 0 the construction is complete
-		return $this;
-	}
-
-	public function isActive() {
-		return $this->getActive();
-	}
-	
-	public function abandon($damage = 1) {
-		if ($this->isActive()) {
-			$this->setActive(false);
-			$this->setCondition(-$damage);
-		}
-		$this->setWorkers(0);
-		return $this;
-	}
-	
-	public function getNametrans() {
-		return 'Exhibit.'.$this->getName();
-	}
-	
-	public function getWhere() {
-		if ($this->getSettlement() != false) {
-			return $this->getSettlement()
-		} else {
-			return 'Exhibit.near.'.$this->getSettlement()
-		}
-	}
 }
