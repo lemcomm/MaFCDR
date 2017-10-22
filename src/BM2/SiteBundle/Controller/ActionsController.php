3928b5ead4b37819592e0690607a17c3e734b60f
Andrew <Zanaras@users.noreply.github.com>
2017-10-22 22:17:21 +0900

    Update ActionsController.php

--- src/BM2/SiteBundle/Controller/ActionsController.php
+++ src/BM2/SiteBundle/Controller/ActionsController.php
@@ -854,19 +854,35 @@ class ActionsController extends Controller {
      */
 	public function offersAction(Request $request) {
 		list($character, $settlement) = $this->get('dispatcher')->gateway('personalOffersTest', true);
-		$welcomers = [];
-		
-		if ($character->findRealms()) {
-			foreach ($character->findRealms() as $realm) {
-				foreach ($realm->getPositions() as $position) {
-					if ($position->isWelcomer()) {
-						$weclomers[] = $position;
-					}
-				}
+		$em = $this->getDoctrine()->getManager();
+		$depth = 1;
+		echo $settlement->getRealm()->getName();
+		if ($settlement->getRealm()->getSuperior()) {
+			$depth = 2;
+			echo $settlement->getRealm()->getSuperior()->getName();
+			if (($settlement->getRealm()->findUltimate() != $settlement->getRealm()) AND ($settlement->getRealm()->findUltimate() != $settlement->getRealm()->getSuperior())) {
+				$depth = 3;
 			}
-		}			
+		}
+		switch ($depth) {
+			case 3:
+				$query = $em->createQuery('SELECT p FROM BM2SiteBundle:RealmPosition p WHERE (p.realm = :realm AND p.welcomer = true) OR (p.realm = :superior AND p.welcomer = true) OR (p.realm = :ultimate AND p.welcomer = true)');
+				$query->setParameter('realm', $settlement->getRealm());
+				$query->setParameter('superior', $settlement->getRealm()->getSuperior());
+				$query->setParameter('ultimate', $settlement->getRealm()->findUltimate());
+				break;
+			case 2:
+				$query = $em->createQuery('SELECT p FROM BM2SiteBundle:RealmPosition p WHERE (p.realm = :realm AND p.welcomer = true) OR (p.realm = :superior AND p.welcomer = true)');
+				$query->setParameter('realm', $settlement->getRealm());
+				$query->setParameter('superior', $settlement->getRealm()->findUltimate());
+				break;
+			case 1:
+				$query = $em->createQuery('SELECT p FROM BM2SiteBundle:RealmPosition p WHERE p.realm = :realm AND p.welcomer = true');
+				$query->setParameter('realm', $settlement->getRealm());
+				break;
+		}
 
-		$form = $this->createForm(new KnightOfferType($settlement, $welcomers));
+		$form = $this->createForm(new KnightOfferType($settlement, $query->getResult()));
 		$form->handleRequest($request);
 		if ($form->isValid()) {
 			$data = $form->getData();
@@ -894,6 +910,7 @@ class ActionsController extends Controller {
 					$offer->setSettlement($settlement);
 					$offer->setDescription($data['intro']);
 					$offer->setGiveSettlement($data['givesettlement']);
+					$offer->setWelcomers($data['welcomers']);
 					$em->persist($offer);
 					if ($data['givesettlement'] == false) {
 						foreach ($data['soldiers'] as $soldier) {
