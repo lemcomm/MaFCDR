e655670c23b13d9f5c7ca93c04817042b40d8b42
Andrew <Zanaras@users.noreply.github.com>
2017-12-17 23:04:41 +0900

    Update LoadPermissionData.php

--- src/BM2/SiteBundle/DataFixtures/ORM/LoadPermissionData.php
+++ src/BM2/SiteBundle/DataFixtures/ORM/LoadPermissionData.php
@@ -15,17 +15,28 @@ class LoadPermissionData extends AbstractFixture implements OrderedFixtureInterf
 		'settlement' => array(
 			'visit'    	=> array('use_value'=>false, 'use_reserve'=>false),
 			'docks'    	=> array('use_value'=>false, 'use_reserve'=>false),
-			'resupply'  => array('use_value'=>true, 'use_reserve'=>false),
-			'mobilize'  => array('use_value'=>true, 'use_reserve'=>true),
-			'construct' => array('use_value'=>false, 'use_reserve'=>true),
-			'recruit'   => array('use_value'=>true, 'use_reserve'=>false),
-			'trade'    	=> array('use_value'=>false, 'use_reserve'=>false),
+			'describe'	=> array('use_value'=>false, 'user_reserve'=>false),
+			'resupply'	=> array('use_value'=>true, 'use_reserve'=>false),
+			'mobilize'	=> array('use_value'=>true, 'use_reserve'=>true),
+			'construct'	=> array('use_value'=>false, 'use_reserve'=>true),
+			'recruit'	=> array('use_value'=>true, 'use_reserve'=>false),
+			'trade'    	=> array('use_value'=>false, 'use_reserve'=>false)
 		),
 		'realm' => array(
 			'expel'   	=> array('use_value'=>false, 'use_reserve'=>false),
+			'describe'	=> array('use_value'=>false, 'use_reserve'=>false),
 			'diplomacy'	=> array('use_value'=>false, 'use_reserve'=>false),
 			'laws'		=> array('use_value'=>false, 'use_reserve'=>false),
 			'positions'	=> array('use_value'=>false, 'use_reserve'=>false),
+			'wars'		=> array('use_value'=>false, 'use_reserve'=>false)
+		),
+		'place' => array(
+			'visit'		=> array('use_value'=>false, 'user_reserve'=>false),
+			'docks'		=> array('use_value'=>false, 'user_reserve'=>false),
+			'describe'	=> array('use_value'=>false, 'user_reserve'=>false),
+			'resupply'	=> array('use_value'=>true, 'user_reserve'=>false),
+			'mobilize'	=> array('use_value'=>true, 'user_reserve'=>true),
+			'construct'	=> array('use_value'=>false, 'user_reserve'=>true)
 		)
 	);
 
@@ -42,7 +53,11 @@ class LoadPermissionData extends AbstractFixture implements OrderedFixtureInterf
 	public function load(ObjectManager $manager) {
 		foreach ($this->permissions as $class=>$members) {
 			foreach ($members as $name=>$data) {
-				$perm = new Permission();
+				$perm = $manager->getRepositiory('BM2SiteBundle:Permission')->findOneBy(array('name'=>$name, 'class'=>$class));
+				if (!$perm) {
+					$perm = new Permission();
+					$manager->persist($perm);
+				}
 				$perm->setName($name);
 				$perm->setTranslationString('perm.'.$name);
 				$perm->setClass($class);
