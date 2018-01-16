3e67e8c8a35476a33f33a9d40f703f51d9841074
Andrew <Zanaras@users.noreply.github.com>
2018-01-17 03:42:09 +0900

    Update PlaceManageType.php

--- src/BM2/SiteBundle/Form/PlaceManageType.php
+++ src/BM2/SiteBundle/Form/PlaceManageType.php
@@ -29,13 +29,14 @@ class PlaceManageType extends AbstractType {
 		$isNew = $this->isNew;
 		$isOwner = $this->isOwner;
 		$description = $this->description;
-		if ($this->isOwner OR $this->isNew) {
+		if ($isOwner OR $isNew) {
 			$builder->add('name', 'text', array(
 				'label'=>'names.name', 
 				'required'=>true, 
 				'attr' => array(
 					'size'=>20, 
-					'maxlength'=>40
+					'maxlength'=>40,
+					'title'=>'help.new.name'
 				)
 			));
 			$builder->add('formal_name', 'text', array(
@@ -43,36 +44,47 @@ class PlaceManageType extends AbstractType {
 				'required'=>true, 
 				'attr' => array(
 					'size'=>40, 
-					'maxlength'=>160
+					'maxlength'=>160,
+					'title'=>'help.new.formalname'
 				)
 			));
 		}
-		if ($this->isNew) {
+		if ($isNew) {
 			# This isn't going to work. Prep the list in the Controller, submit it through the form, then reapply it on the other side.
 			$builder->add('type', 'entity', array(
 				'label'=>'type.label',
 				'required'=>true,
 				'placeholder' => 'type.empty',
-				'attr' => array('title'=>'place.help.type'),
+				'attr' => array('title'=>'help.new.type'),
 				'class' => 'BM2SiteBundle:PlaceType',
 				'choice_translation_domain' => true,
 				'choice_label' => 'name',
-				'choices' => $types
+				'choices' => $types,
+				'group_by' => function($val, $key, $index) {
+					if ($val->getRequires() == NULL) { 
+						return 'by.none';
+					} else {
+						return 'by.'.$val->getRequires();
+					}
+				}
 			));
 		}
 		$builder->add('short_description', 'textarea', array(
 			'label'=>'description.short',
+			'attr' => array('title'=>'help.new.shortdesc'),
 			'required'=>true,
 		));
 		if ($description) {
 			$builder->add('description', 'textarea', array(
 				'label'=>'description.full',
+				'attr' => array('title'=>'help.new.longdesc'),
 				'data_class'=> NULL,
 				'required'=>true,
 			));
 		} else { 
 			$builder->add('description', 'textarea', array(
 				'label'=>'description.full',
+				'attr' => array('title'=>'help.new.longdesc'),
 				'data'=>$description,
 				'data_class'=> NULL,
 				'required'=>true,
