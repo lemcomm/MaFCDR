708a27ea97f9ab60b00356ec8684832d293648c6
Andrew <Zanaras@users.noreply.github.com>
2018-04-21 23:56:52 +0900

    Update HouseCreationType.php

--- src/BM2/SiteBundle/Form/HouseCreationType.php
+++ src/BM2/SiteBundle/Form/HouseCreationType.php
@@ -8,39 +8,55 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class HouseCreationType extends AbstractType {
 
+	private $house;
+	
+	public function __construct($name = NULL, $desc = NULL, $priv = NULL, $secret = NULL) {
+		$this->name = $name;
+		$this->desc = $desc;
+		$this->priv = $priv;
+		$this->secret = $secret;
+	}		
+	
 	public function configureOptions(OptionsResolver $resolver) {
 		$resolver->setDefaults(array(
 			'intention'       => 'housecreation_78315',
 			'translation_domain' => 'politics'
 		));
 	}
-
+	
 	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$name = $this->name;
+		$desc = $this->desc;
+		$priv = $this->priv;
+		$secret = $this->secret;
 		$builder->add('name', 'textfield', array(
 			'label'=>'house.setup.name',
 			'required'=>true,
+			'data'=>$name,
 			'attr' => array('size'=>30, 'maxlength'=>80, 'title'=>'newcharacter.help.name')
 		));
 		$builder->add('description', 'textarea', array(
 			'label'=>'house.setup.description',
 			'trim'=>true,
-			'required'=>false
+			'required'=>false,
+			'data'=>$desc
 		));
 		$builder->add('private', 'textarea', array(
 			'label'=>'house.setup.private',
 			'trim'=>true,
-			'required'=>false
+			'required'=>false,
+			'data'=>$priv
 		));
 		$builder->add('secret', 'textarea', array(
 			'label'=>'house.setup.secret',
 			'trim'=>true,
-			'required'=>false
+			'required'=>false,
+			'data'=>$secret
 		));
 
 		$builder->add('submit', 'submit', array('label'=>'house.setup.submit'));
 	}
-
-	# What the below actually do anyways? I've a feeling it's something to do with the automated testing tools, but I am not sure. --Andrew
+	
 	public function getName() {
 		return 'housecreate';
 	}
