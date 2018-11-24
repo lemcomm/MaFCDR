8597e36b8a06c5f0625fc68f9a79d9bb584c9b1c
Andrew <Zanaras@users.noreply.github.com>
2018-11-23 21:59:32 -0400

    Update Siege.php

--- src/BM2/SiteBundle/Entity/Siege.php
+++ src/BM2/SiteBundle/Entity/Siege.php
@@ -2,7 +2,27 @@
 
 namespace BM2\SiteBundle\Entity;
 
+use Doctrine\ORM\EntityRepository;
+
 class Siege {
 
-
+	public function getLeader() {
+		$leader = null;
+		foreach ($this->groups as $group) {
+			if ($group->isAttacker) {
+				$leader = $group->getLeader();
+			}
+		}
+		return $leader;
+	}
+	
+	public function setLeader($character) {
+		foreach ($this->groups as $group) {
+			if ($group->isAttacker) {
+				$group->setLeader($character);
+			}
+		}
+		$this->getDoctrine()->getManager()->flush();
+	}
+  
 }
