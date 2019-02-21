3d710084a550b0f0dfc0994741b12089f0e7149a
Andrew <Zanaras@users.noreply.github.com>
2019-02-20 22:51:39 -0400

    Create ActionManager.php

---
+++ src/BM2/SiteBundle/Service/ActionManager.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace BM2\SiteBundle\Service;
+
+use BM2\SiteBundle\Entity\Action;
+
+/*
+This mostly exists to get the queue function out of ActionResolution, in order to avoid circular dependencies.
+*/
+
+class ActionManager {
+
+	private $em;
+
+	public function __construct(EntityManager $em) {
+		$this->em = $em;
+	}
+
+	public function queue(Action $action) {
+		$action->setStarted(new \DateTime("now"));
+
+		// store in database and queue
+		$success = true;
+		$max=0;
+		foreach ($action->getCharacter()->getActions() as $act) {
+			if ($act->getPriority()>$max) {
+				$max=$act->getPriority();
+			}
+		}
+		$action->setPriority($max+1);
+
+		// some defaults, otherwise I'd have to set it explicitly everywhere
+		if ($action->getHidden()===null) {
+			$action->setHidden(false);
+		}
+		if ($action->getHourly()===null) {
+			$action->setHourly(false);
+		}
+		if ($action->getCanCancel()===null) {
+			$action->setCanCancel(true);
+		}
+		$this->em->persist($action);
+
+		$this->em->flush();
+
+		return array('success'=>$success);
+	}
+
+}
