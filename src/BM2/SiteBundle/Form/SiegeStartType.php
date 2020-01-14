23365c094e862a58023f583bde3b029c3ed3ffca
Andrew <Zanaras@users.noreply.github.com>
2020-01-14 23:21:00 +0000

    Update SiegeStartType.php

--- src/BM2/SiteBundle/Form/SiegeStartType.php
+++ src/BM2/SiteBundle/Form/SiegeStartType.php
@@ -4,13 +4,21 @@ namespace BM2\SiteBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SiegeStartType extends AbstractType {
 
-	public function __construct($settlement, $place) {
+	private $settlement;
+	private $places;
+
+	public function __construct($settlement = null, $places = null) {
 		$this->settlement = $settlement;
-		$this->place = $place;
+		$this->places = $places;
 	}
 
 	public function configureOptions(OptionsResolver $resolver) {
@@ -21,19 +29,37 @@ class SiegeStartType extends AbstractType {
 	}
 
 	public function buildForm(FormBuilderInterface $builder, array $options) {
-		$options = array($settlement, $place);
-		$builder->add('target', ChoiceType::class, array(
-			'required'=>true,
-			'choices' => $options,
-			'placeholder'=>'military.siege.locations.none',
-			'label'=> 'military.siege.locations.name'
-		));
-		$builder->add('sure', CheckboxType::class, array(
-			'label' => 'areyousure',
-			'required' => true
-		));
+		$settlement = $this->settlement;
+		$places = $this->places;
 
-		$builder->add('submit', 'submit', array('label'=>'button.submit'));
+		if ($settlement) {
+			$builder->add('settlement', CheckboxType::class, array(
+				'required'=>false,
+				'label'=> 'military.siege.locations.name'
+			));
+		} else {
+			$builder->add('settlement', HiddenType::class, array(
+				'data'=>false
+			));
+		}
+
+		if ($places) {
+			$builder->add('place', ChoiceType::class, array(
+				'required'=>false,
+				'choices' => $places,
+				'choice_label' => 'name',
+				'placeholder'=>'military.siege.locations.none',
+				'label'=> 'military.siege.locations.name'
+			));
+		} else {
+			$builder->add('places', HiddenType::class, array(
+				'data'=>false
+			));
+		}
+
+		if ($settlement || $places) {
+			$builder->add('submit', 'submit', array('label'=>'button.submit'));
+		}
 	}
 
 	public function getName() {
