201c236b8768b278ca943530af0797593bb42dcb
Andrew <Zanaras@users.noreply.github.com>
2020-05-05 21:48:53 +0100

    Update SettlementController.php

--- src/BM2/SiteBundle/Controller/SettlementController.php
+++ src/BM2/SiteBundle/Controller/SettlementController.php
@@ -5,7 +5,6 @@ namespace BM2\SiteBundle\Controller;
 use BM2\SiteBundle\Entity\Character;
 use BM2\SiteBundle\Entity\Settlement;
 use BM2\SiteBundle\Form\SettlementPermissionsSetType;
-use BM2\SiteBundle\Form\SoldiersManageType;
 use BM2\SiteBundle\Form\DescriptionNewType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -44,11 +43,11 @@ class SettlementController extends Controller {
 			$form_map = $this->createFormBuilder()->add('settlement_id', 'hidden', array(
 				'constraints' => array() // TODO: constrained to available settlements
 			))->getForm();
-			$details['startme'] = $form_map->createView();			
+			$details['startme'] = $form_map->createView();
 		}
 
 		// FIXME: shouldn't this use geodata?
-		$query = $em->createQuery('SELECT s.id, s.name, ST_Distance(y.center, x.center) AS distance, ST_Azimuth(y.center, x.center) AS direction 
+		$query = $em->createQuery('SELECT s.id, s.name, ST_Distance(y.center, x.center) AS distance, ST_Azimuth(y.center, x.center) AS direction
 			FROM BM2SiteBundle:Settlement s JOIN s.geo_data x, BM2SiteBundle:GeoData y WHERE y=:here AND ST_Touches(x.poly, y.poly)=true');
 		$query->setParameter('here', $settlement);
 		$neighbours = $query->getArrayResult();
@@ -122,7 +121,7 @@ class SettlementController extends Controller {
 				} else {
 					$FoodSupply = 1.0;
 				}
-			} 
+			}
 		}
 
 		return array(
@@ -143,113 +142,6 @@ class SettlementController extends Controller {
 			'heralds' => $heralds
 		);
 	}
-
-
-	/**
-	  * @Route("/{id}/soldiers/{start}", requirements={"id"="\d+", "start"="\d+"}, defaults={"start"=0})
-	  * @Template
-	  */
-	public function soldiersAction($id, $start, Request $request) {
-		$em = $this->getDoctrine()->getManager();
-
-		list($character, $settlement) = $this->get('dispatcher')->gateway('personalMilitiaTest', true);
-		if (! $character instanceof Character) {
-			return $this->redirectToRoute($character);
-		}
-		if (!$settlement) {
-			throw $this->createNotFoundException('error.notfound.settlement');
-		}
-		if ($settlement->getId() != $id) {
-			throw new AccessDeniedHttpException('error.noaccess.settlement');
-		}
-		if ($character->getPrisonerOf()) {
-			$others = array($character->getPrisonerOf());
-		} else {
-			$others = $this->get('dispatcher')->getActionableCharacters(true);
-		}
-
-		$resupply = $this->get('military_manager')->findAvailableEquipment($settlement, false);
-		$training = $this->get('military_manager')->findAvailableEquipment($settlement, true);
-
-		// this duplicates functionality from the Soldier Entity (e.g. determine who is militia and who recruit),
-		// but I think we need it for memory reasons, since loading all and then slicing is a ton less efficient
-		$query = $em->createQuery("SELECT count(s) FROM BM2SiteBundle:Soldier s WHERE s.base = :here AND s.training_required <= 0")			
-			->setParameter('here', $settlement);
-		$total_soldiers = $query->getSingleScalarResult();
-		$query = $em->createQuery("SELECT s FROM BM2SiteBundle:Soldier s WHERE s.base = :here AND s.training_required <= 0 ORDER BY s.name ASC")
-			->setParameter('here', $settlement)
-			->setFirstResult($start)
-			->setMaxResults($this->slice_size);
-		$militia_slice = array();
-		foreach ($query->getResult() as $soldier) {
-			$militia_slice[$soldier->getId()] = $soldier;
-		}
-
-
-		$form = $this->createForm(new SoldiersManageType($em, $militia_slice, $resupply, $training, $others, $settlement));
-		$form->handleRequest($request);
-		if ($form->isValid()) {
-			$data = $form->getData();
-
-			$this->get('military_manager')->manage($militia_slice, $data, $settlement, $character);
-			$em->flush();
-			$this->get('appstate')->setSessionData($character); // update, because maybe we changed our soldiers count
-			return $this->redirect($request->getUri());
-		}
-
-		return array(
-			'settlement' => $settlement,
-			'soldiers' => $militia_slice,
-			'recruits' => $settlement->getRecruits(),
-			'resupply' => $resupply,
-			'training' => $training,
-			'total_soldiers' => $total_soldiers,
-			'start' => $start,
-			'slice' => $this->slice_size,
-			'form' => $form->createView(),
-			'limit' => $this->get('appstate')->getGlobal('pagerlimit', 100)
-		);
-	}
-
-
-	/**
-	* @Route("/canceltraining")
-	*/
-	public function cancelTrainingAction(Request $request) {
-		if ($request->isMethod('POST') && $request->request->has("settlement") && $request->request->has("recruit")) {
-			list($character, $settlement) = $this->get('dispatcher')->gateway('personalMilitiaTest', true);
-			if (! $character instanceof Character) {
-				return $this->redirectToRoute($character);
-			}
-
-			$em = $this->getDoctrine()->getManager();
-			$recruit = $em->getRepository('BM2SiteBundle:Soldier')->find($request->request->get('recruit'));
-			if (!$recruit || !$recruit->isRecruit() || $recruit->getBase()!=$settlement) {
-				throw $this->createNotFoundException('error.notfound.recruit');
-			}
-
-			// return his equipment to the stockpile:
-			$this->get('military_manager')->returnItem($settlement, $recruit->getWeapon());
-			$this->get('military_manager')->returnItem($settlement, $recruit->getArmour());
-			$this->get('military_manager')->returnItem($settlement, $recruit->getEquipment());
-
-			if ($recruit->getOldWeapon() || $recruit->getOldArmour() || $recruit->getOldEquipment()) {
-				// old soldier - return to militia with his old stuff
-				$recruit->setWeapon($recruit->getOldWeapon());
-				$recruit->setArmour($recruit->getOldArmour());
-				$recruit->setEquipment($recruit->getOldEquipment());
-				$recruit->setTraining(0)->setTrainingRequired(0);
-				$this->get('history')->addToSoldierLog($recruit, 'traincancel');
-			} else {
-				// fresh recruit - return to workforce
-				$settlement->setPopulation($settlement->getPopulation()+1);
-				$em->remove($recruit);
-			}
-			$em->flush();
-		}
-		return new Response();
-	}
-
 
 	/**
 	  * @Route("/{id}/permissions", requirements={"id"="\d+"})
