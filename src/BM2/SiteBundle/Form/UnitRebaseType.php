9d15e99448887956650d7e04a82767f62315c39e
Andrew <Zanaras@users.noreply.github.com>
2020-06-01 19:57:53 +0100

    Create UnitRebaseType.php

---
+++ src/BM2/SiteBundle/Form/UnitRebaseType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace BM2\SiteBundle\Form;
+
+use BM2\SiteBund\Entity\Settlement;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityRepository;
+
+class UnitRebaseType extends AbstractType {
+
+	public function __construct($options) {
+		$this->options = $options;
+	}
+
+	public function configureOptions(OptionsResolver $resolver) {
+		$resolver->setDefaults(array(
+			'intention'       	=> 'rebase_12345',
+			'translation_domain' => 'actions',
+		));
+	}
+
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$options = $this->options;
+
+		$builder->add('settlement', Settlement::class, array(
+			'label' => 'unit.rebase.settlement',
+			'multiple'=>false,
+			'expanded'=>false,
+			'class'=>'BM2SiteBundle:Settlement',
+                        'choice_label'=>'name',
+                        'query_builder'=>function(EntityRepository $er) use ($options) {
+				$qb = $er->createQueryBuilder('s');
+				$qb->where('s.id in (:options)')->setParameter('options', $options);
+				$qb->orderBy('s.name');
+				return $qb;
+			},
+                        'placeholder' => 'unit.rebase.none'
+		));
+
+		$builder->add('submit', SubmitType::class, array(
+                        'label'=>'button.submit',
+                        'translation_domain'=>'settings',));
+	}
+}
