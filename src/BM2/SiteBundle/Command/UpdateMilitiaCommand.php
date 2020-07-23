69127cc6e2d4cf6c56af2c15392476307201048a
Andrew <Zanaras@users.noreply.github.com>
2020-07-23 07:24:49 +0100

    Update UpdateMilitiaCommand.php

--- src/BM2/SiteBundle/Command/UpdateMilitiaCommand.php
+++ src/BM2/SiteBundle/Command/UpdateMilitiaCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 use BM2\SiteBundle\Entity\Settlement;
 
@@ -27,50 +28,67 @@ class UpdateMilitiaCommand extends ContainerAwareCommand {
 		$em = $this->getContainer()->get('doctrine')->getManager();
 		$mm = $this->getContainer()->get('military_manager');
                 $source = $input->getArgument('target');
+                $stopwatch = new Stopwatch();
+		$execLimit = 50;
 
                 if ($source != 'all') {
                         $output->writeln("Looking for Settlement #".$source);
-                        $target = $em->getRepository('BM2SiteBundle:Settlement')->findOneById($source);
+			$target = $em->getRepository('BM2SiteBundle:Settlement')->findOneById($source);
                         if ($target instanceof Settlement) {
                                 $output->writeln('Settlement found...');
                         } else {
                                 throw new \Exception("Cannot find settlement. Please check the id and try again.");
                         }
-                        $all = new ArrayCollection();
-                        $all->add($target);
+			$output->writeln("Converting selection to script expectations...");
+			# The way this script is written, we have to do a DQL query so we can iterate.
+			$query = $em->createQuery('SELECT s FROM BM2SiteBundle:Settlement s WHERE s.id = :id');
+			$query->setParameters(["id"=>$source]);
+			$countQuery = $em->createQuery('SELECT COUNT(s.id) FROM BM2SiteBundle:Settlement s WHERE s.id = :id');
+			$countQuery->setParameters(["id"=>$source]);
                 } else {
                         $output->writeln('All detected, fetching repository...');
-                        $all = new ArrayCollection($em->getRepository('BM2SiteBundle:Settlement')->findAll());
+			$query = $em->createQuery('SELECT s FROM BM2SiteBundle:Settlement s');
+			$countQuery = $em->createQuery('SELECT COUNT(s.id) FROM BM2SiteBundle:Settlement s');
                 }
+		$stopwatch->start('updateSoldiers');
 
-                $allcount = $all->count();
-                $output->writeln('Generating units for '.$allcount.' settlements.');
+                $allCount = $countQuery->getSingleScalarResult();
+                $output->writeln('Generating units for '.$allCount.' settlements.');
+		$total = 0;
                 $progress = 0;
-                $unitcount = 0;
-                $counter = 1;
+                $unitCount = 0;
 
-                foreach ($all as $s) {
-                        if ($s->getSoldiers()->isEmpty()) {
-                                $progress++;
-                                $output->writeln('No units needed for '.$s->getName().'. ('.$progress.'/'.$allcount.')');
-                        } else {
-                                $progress++;
-                                $output->writeln('Creating unit(s) for '.$c->getName().'... ('.$progress.'/'.$allcount.')');
-                                $total = $mm->convertToUnit(null, $s, null, true);
-                                $unitcount += $total;
-                                $output->writeln('Created '.$total.' units. '.$unitcount.' so far this execution.');
-                        }
-                        if ($alter) {
-                                $counter++;
-                                $alter = false;
-                        }
-                        if ($counter == 200) {
-                                $output->writeln('Execution optimization limit reached. Rerun this command to continue.');
-                                break;
-                        }
-                }
+		while ($progress < $allCount) {
+			$output->writeln("Beginning execution loop...");
+			$em->clear();
+			$executions = 0;
 
-                $query = $em->createQuery('UPDATE BM2SiteBundle:Soldier s SET s.liege = NULL, s.character = NULL, s.base = NULL, s.group = NULL, s.assigned_since = NULL WHERE s.unit IS NOT NULL and s.base_id IS NOT NULL');
-                $query->execute();
+			$result = $query->iterate();
+			while (($row = $result->next()) !== false AND $executions < $execLimit AND $progress < $allCount) {
+				$s = $row[0];
+	                        if ($s->getSoldiersOld()->isEmpty()) {
+	                                $progress++;
+	                                $output->writeln('No units needed for '.$s->getName().'. ('.$progress.'/'.$allCount.')');
+					$executions++;
+	                        } else {
+	                                $progress++;
+	                                $output->writeln('Creating unit(s) for '.$s->getName().'... ('.$progress.'/'.$allCount.')');
+	                                $total = $mm->convertToUnit(null, $s, null, true);
+	                                $unitCount += $total;
+	                                $output->writeln('Created '.$total.' units. '.$unitCount.' so far this execution.');
+					$executions++;
+	                        }
+	                }
+			$em->flush();
+			if ($progress >= $allCount) {
+				break;
+			}
+		}
+
+		$output->writeln('Removing settlement associations for soldiers now assigned to units.');
+		$query = $em->createQuery('UPDATE BM2SiteBundle:Soldier s SET s.liege = NULL, s.character = NULL, s.base = NULL, s.group = NULL, s.assigned_since = NULL WHERE s.unit IS NOT NULL and s.base IS NOT NULL');
+		$query->execute();
+		$event = $stopwatch->stop('updateSoldiers');
+		$output->writeln('End of update reached. '.$unitCount.' Units for '.$progress.' Settlements in '.($event->getDuration()/1000).' seconds.');
 	}
 }
