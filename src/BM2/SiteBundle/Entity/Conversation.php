025e43aa848f2a2f6f72036705f8847de5015204
Andrew <Zanaras@users.noreply.github.com>
2020-07-24 20:29:26 +0100

    Update Conversation.php

--- src/BM2/SiteBundle/Entity/Conversation.php
+++ src/BM2/SiteBundle/Entity/Conversation.php
@@ -21,5 +21,29 @@ class Conversation {
                 $criteria = Criteria::create()->where(Criteria::expr()->eq("active", true));
                 return $this->getPermissions()->matching($criteria);
         }
+
+        public function findCharPermissions($char) {
+                $criteria = Criteria::create()->where(Criteria::expr()->eq("character", $char));
+                return $this->getPermissions()->matching($criteria);
+        }
+
+        public function findActiveCharPermission($char) {
+                $criteria = Criteria::create()->where(Criteria::expr()->eq("character", $char))->andWhere(Criteria::expr()->eq("active", true));
+                return $this->getPermissions()->matching($criteria);
+        }
+
+        public function findMessages($char) {
+                $perms = $this->findCharPermissions($char);
+                $all = new ArrayCollection();
+                foreach ($this->getMessages() as $msg) {
+                        foreach ($perms as $perm) {
+                                if ($perm->getStart() <= $msg->getSent() AND ($msg->getSent() <= $perm->getEnd() OR $perm->getActive())) {
+                                        $all->add($msg);
+                                        break;
+                                }
+                        }
+                }
+                return $all;
+        }
         
 }
