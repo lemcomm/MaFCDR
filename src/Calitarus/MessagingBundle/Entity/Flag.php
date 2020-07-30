e0321c4c7c75f7c6db0bd1f5846fd46d8cdd0069
Andrew <Zanaras@users.noreply.github.com>
2020-07-30 23:35:32 +0100

    Add files via upload

---
+++ src/Calitarus/MessagingBundle/Entity/Flag.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Calitarus\MessagingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Flag
+ */
+class Flag
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Flag
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
