7ac0ab3a9aadb0893511b3688f85f74a60649643
Andrew <Zanaras@users.noreply.github.com>
2020-07-30 22:40:48 +0100

    Update ConversationController.php

--- src/BM2/SiteBundle/Controller/ConversationController.php
+++ src/BM2/SiteBundle/Controller/ConversationController.php
@@ -4,8 +4,10 @@ namespace BM2\SiteBundle\Controller;
 
 use BM2\SiteBundle\Entity\Character;
 use BM2\SiteBundle\Entity\Conversation;
+use BM2\SiteBundle\Entity\Conversationpermission;
 use BM2\SiteBundle\Entity\Message;
 use BM2\SiteBundle\Entity\Realm;
+use BM2\SiteBundle\Form\AddParticipantType;
 use BM2\SiteBundle\Form\MessageReplyType;
 use BM2\SiteBundle\Form\NewConversationType;
 
@@ -96,249 +98,9 @@ class ConversationController extends Controller {
 	}
 
 	/**
-	  * @Route("/read/{conv}", name="maf_conv_read", requirements={"conv"="\d+"})
+	  * @Route("/new", name="maf_conv_new")
+	  * @Route("/new/r/{realm}", name="maf_conv_realm_new")
 	  */
-	public function readAction(Conversation $conv) {
-                $char = $this->get('dispatcher')->gateway('conversationSingleTest', false, true, false, $conv);
-                if (! $char instanceof Character) {
-                        return $this->redirectToRoute($char);
-                }
-
-		$messages = $conv->findMessages($char);
-		$perms = $conv->findCharPermissions($char);
-		$lastPerm = $perms->last();
-		$unread = $lastPerm->getUnread();
-		$total = $messages->count();
-
-		if ($unread) {
-			$lastPerm->setUnread(0);
-			$i = 0;
-			foreach ($messages as $m) {
-				$i++;
-				if ($i == $total - $unread) {
-					$last = $m->getSent();
-					break;
-				}
-			}
-		} else {
-			$unread = 0;
-			$last = NULL;
-		}
-		if ($lastPerm->getActive()) {
-			$lastPerm->setLastAccess(new \DateTime('now'));
-		}
-
-		#Find the timestamp of the last read message.
-
-		$veryold = new \DateTime('now');
-		$veryold->sub(new \DateInterval("P30D")); // TODO: make this user-configurable
-
-		return $this->render('Conversation/conversation.html.twig', [
-			'conversation' => $conv,
-			'messages' => $messages,
-			'total' => $total,
-			'unread' => $unread,
-			'veryold' => $veryold,
-			'last' => $last,
-		]);
-	}
-
-	/**
-	  * @Route("/participants/{conv}", name="maf_conv_participants", requirements={"conv"="\d+"})
-	  */
-	public function participantsAction(Conversation $conv) {
-                $char = $this->get('dispatcher')->gateway('conversationManageTest', false, true, false, $conv);
-                if (! $char instanceof Character) {
-                        return $this->redirectToRoute($char);
-                }
-
-		$perms = $conv->findRelevantPermissions($char); #Get what permissions we're aware of.
-
-		$manager = false;
-		$owner = false;
-		$form = null;
-		if (!$conv->getRealm()) {
-			if ($me = $conv->findActiveCharpermission($char)) {
-				$manager = $me->getManager();
-				$owner = $me->getOwner();
-			}
-		}
-
-		return $this->render('Conversation/participants.html.twig', [
-			'conv' =>$conv,
-			'perms'=>$perms,
-			'manager'=>$manager,
-			'owner'=>$owner,
-			'me'=>$char,
-		]);
-	}
-
-	/**
-	  * @Route("/{conv}/add", name="maf_conv_add", requirements={"conv"="\d+"})
-	  */
-	public function addParticipantsAction(Conversation $conv) {
-                $char = $this->get('dispatcher')->gateway('conversationAddTest', false, true, false, $conv);
-                if (! $char instanceof Character) {
-                        return $this->redirectToRoute($char);
-                }
-
-		# Dispatcher means we already know this user is either a manager or an owner, thus, they have add rights.
-		$perms = $conv->findRelevantPermissions($char);
-		$contacts = $this->get('conversation_manager')->getLegacyContacts($char);
-		foreach ($perms as $perm) {
-			if ($contacts->contains($perm->getCharacter())) {
-				$contacts->remove($perm->getCharacter()); #Remove people who already have permissions.
-			}
-		}
-		$form = new AddParticipantType($contacts);
-
-		$form->handleRequest($request);
-		if ($form->isValid()) {
-			$data = $form->getData();
-			$em = $this->getDoctrine()->getManager();
-			$now = new \DateTime("now");
-			$added = new ArrayCollection();
-			foreach($data['characters'] as $char) {
-				# Double check we can actually add this person.
-				if ($contacts->contains($char)) {
-					$perm = new ConversationPermission();
-					$em->persist($perm);
-					$perm->setConversation($conv);
-					$perm->setCharacter($char);
-					$perm->setStartTime($now);
-					$perm->setActive(true);
-					$perm->setUnread(0);
-					$perm->setManager(false);
-					$perm->setOwner(false);
-					$added->add($char);
-				}
-			}
-			$message = $this->get('conversation_manager')->addSystemMessage($conv, 'newperms', $added, $char, false);
-			$this->em->flush();
-			return new RedirectResponse($this->generateUrl('maf_conv_read', ['conv' => $conv->getId()]).'#'.$message->getId());
-		}
-		return $this->render('Conversation/add.html.twig', [
-			'conv' =>$conv,
-			'perms'=>$perms,
-			'manager'=>$manager,
-			'owner'=>$owner,
-		]);
-	}
-
-	/**
-	  * @Route("/{conv}/change/{perm}/{var}", name="maf_conv_change", requirements={"conv"="\d+", "perm"="\d+", "var"="\d+"})
-	  */
-	public function changePermissionAction(Conversation $conv, ConversationPermission $perm, $var) {
-                $char = $this->get('dispatcher')->gateway('conversationChangeTest', false, true, false, $conv);
-                if (! $char instanceof Character) {
-                        return $this->redirectToRoute($char);
-                }
-
-		if ($me = $conv->findActivePermissions($char)) {
-			$em = $this->getDoctrine()->getManager();
-			if ($me->getOwner()) {
-				if (!$perm->getManager()) {
-					if ($var === 0) {
-						$perm->setActive(false);
-						$change = 'permission.demoted.removed';
-						$flush = true;
-						$message = $this->get('conversation_manager')->addSystemMessage($conv, 'removal', $perm->getCharacter(), $char, false);
-
-					} elseif ($var === 1) {
-						$perm->setManager(true);
-						$flush = true;
-						$change = 'permission.promoted.manager';
-					}
-				} elseif ($perm->getOwner()) {
-					if ($var === 0) {
-						$perm->setOwner(false);
-						$flush = true;
-						$change = 'permission.demoted.owner';
-					} elseif ($var === 1) {
-						$change = 'permission.promoted.invalid';
-					}
-				} else {
-					if ($var === 0) {
-						$perm->setManager(false);
-						$flush = true;
-						$change = 'permission.demoted.manager';
-					} elseif ($var === 1) {
-						$perm->setOwner(true);
-						$flush = true;
-						$change = 'permission.promoted.owner';
-					}
-				}
-			} elseif ($me->getManager()) {
-				if ($perm->getOwner() || $perm->getManager()) {
-					$change = 'permission.invalidrequest';
-				} else {
-					if ($var === 0) {
-						$perm->setActive(false);
-						$flush = true;
-						$change = 'permission.demoted.removed';
-					} elseif ($var === 1) {
-						$change = 'permission.nopromoteright';
-					}
-				}
-			} else {
-				$change = 'permission.invalidrequest';
-			}
-			if ($flush) {
-				$em->flush();
-			}
-		}
-
-		$this->addFlash('notice', $this->get('translator')->trans($change, ["%name%"=>$perm->getCharacter()->getName()], 'conversations'));
-
-		return $this->redirectToRoute('maf_conv_participants', ['conv'=>$conv->getId()]);
-	}
-
-	/**
-	  * @Route("/{conv}/leave", name="maf_conv_leave", requirements={"conv"="\d+"})
-	  */
-	public function leaveAction(Conversation $conv) {
-                $char = $this->get('dispatcher')->gateway('conversationLeaveTest', false, true, false, $conv);
-                if (! $char instanceof Character) {
-                        return $this->redirectToRoute($char);
-                }
-
-		if ($conv->findActivePermissions($char)) {
-
-		}
-
-		$active = $conv->findActivePermissions();
-
-		return $this->render('Conversation/conversation.html.twig', [
-			'perms'=>$active,
-			'my_meta'=>$meta
-		]);
-	}
-
-	/**
-	  * @Route("/{conv}/remove", name="maf_conv_remove", requirements={"conv"="\d+"})
-	  */
-	public function removeAction(Conversation $conv) {
-                $char = $this->get('dispatcher')->gateway('conversationRemoveTest', false, true, false, $conv);
-                if (! $char instanceof Character) {
-                        return $this->redirectToRoute($char);
-                }
-
-		if ($conv->findActivePermissions($char)) {
-
-		}
-
-		$active = $conv->findActivePermissions();
-
-		return $this->render('Conversation/conversation.html.twig', [
-			'perms'=>$active,
-			'my_meta'=>$meta
-		]);
-	}
-
-	/**
-		* @Route("/new", name="maf_conv_new")
-		* @Route("/new/r/{realm}", name="maf_conv_realm_new")
-		*/
 	public function newConversationAction(Request $request, Realm $realm=null) {
                 $char = $this->get('dispatcher')->gateway('conversationNewTest');
                 if (! $char instanceof Character) {
@@ -407,9 +169,279 @@ class ConversationController extends Controller {
 		]);
 	}
 
+	/**
+	  * @Route("/{conv}", name="maf_conv_read", requirements={"conv"="\d+"})
+	  */
+	public function readAction(Conversation $conv) {
+                $char = $this->get('dispatcher')->gateway('conversationSingleTest', false, true, false, $conv);
+                if (! $char instanceof Character) {
+                        return $this->redirectToRoute($char);
+                }
+
+		$em = $this->getDoctrine()->getManager();
+		$messages = $conv->findMessages($char);
+		$perms = $conv->findCharPermissions($char);
+		$lastPerm = $perms->last();
+		$unread = $lastPerm->getUnread();
+		$total = $messages->count();
+
+		if ($unread) {
+			$lastPerm->setUnread(0);
+			$i = 0;
+			foreach ($messages as $m) {
+				$i++;
+				if ($i == $total - $unread) {
+					$last = $m->getSent();
+					break;
+				}
+			}
+		} else {
+			$unread = 0;
+			$last = NULL;
+		}
+		if ($lastPerm->getActive()) {
+			$lastPerm->setLastAccess(new \DateTime('now'));
+		}
+		$em->flush();
+
+		#Find the timestamp of the last read message.
+
+		$veryold = new \DateTime('now');
+		$veryold->sub(new \DateInterval("P30D")); // TODO: make this user-configurable
+
+		return $this->render('Conversation/conversation.html.twig', [
+			'conversation' => $conv,
+			'messages' => $messages,
+			'total' => $total,
+			'unread' => $unread,
+			'veryold' => $veryold,
+			'last' => $last,
+			'active'=> $lastPerm->getActive(),
+		]);
+	}
 
 	/**
-	  * @Route("/reply/{conv}", name="maf_conv_reply", requirements={"conv"="\d+"})
+	  * @Route("/{conv}/participants", name="maf_conv_participants", requirements={"conv"="\d+"})
+	  */
+	public function participantsAction(Conversation $conv) {
+                $char = $this->get('dispatcher')->gateway('conversationManageTest', false, true, false, $conv);
+                if (! $char instanceof Character) {
+                        return $this->redirectToRoute($char);
+                }
+
+		$perms = $conv->findRelevantPermissions($char); #Get what permissions we're aware of.
+
+		$manager = false;
+		$owner = false;
+		$form = null;
+		if (!$conv->getRealm()) {
+			if ($me = $conv->findActiveCharpermission($char)) {
+				$manager = $me->getManager();
+				$owner = $me->getOwner();
+			}
+		}
+
+		return $this->render('Conversation/participants.html.twig', [
+			'conv' =>$conv,
+			'perms'=>$perms,
+			'manager'=>$manager,
+			'owner'=>$owner,
+			'active'=>$me,
+			'me'=>$char,
+		]);
+	}
+
+	/**
+	  * @Route("/{conv}/add", name="maf_conv_add", requirements={"conv"="\d+"})
+	  */
+	public function addParticipantsAction(Request $request, Conversation $conv) {
+                $char = $this->get('dispatcher')->gateway('conversationAddTest', false, true, false, $conv);
+                if (! $char instanceof Character) {
+                        return $this->redirectToRoute($char);
+                }
+
+		# Dispatcher means we already know this user is either a manager or an owner, thus, they have add rights.
+		$perms = $conv->findRelevantPermissions($char);
+		$contacts = $this->get('conversation_manager')->getLegacyContacts($char);
+		foreach ($perms as $perm) {
+			if ($perm->getCharacter() && in_array($perm->getCharacter(), $contacts)) {
+				unset($contacts[$perm->getCharacter()->getId()]); #Remove people who already have permissions.
+			}
+		}
+		$form = $this->createForm(new AddParticipantType($contacts));
+
+		$form->handleRequest($request);
+		if ($form->isValid()) {
+			$data = $form->getData();
+			$em = $this->getDoctrine()->getManager();
+			$now = new \DateTime("now");
+			$added = new ArrayCollection();
+			foreach($data['contacts'] as $new) {
+				# Double check we can actually add this person.
+				if (in_array($new, $contacts)) {
+					$perm = new ConversationPermission();
+					$em->persist($perm);
+					$perm->setConversation($conv);
+					$perm->setCharacter($new);
+					$perm->setStartTime($now);
+					$perm->setActive(true);
+					$perm->setUnread(0);
+					$perm->setManager(false);
+					$perm->setOwner(false);
+					$added->add($new);
+				}
+			}
+			$message = $this->get('conversation_manager')->newSystemMessage($conv, 'newperms', $added, $char, false);
+			$em->flush();
+			return new RedirectResponse($this->generateUrl('maf_conv_read', ['conv' => $conv->getId()]).'#'.$message->getId());
+		}
+		return $this->render('Conversation/add.html.twig', [
+			'conv'=>$conv,
+			'perms'=>$perms,
+			'form'=>$form->createView(),
+		]);
+	}
+
+	/**
+	  * @Route("/{conv}/change/{perm}/{var}", name="maf_conv_change", requirements={"conv"="\d+", "perm"="\d+", "var"="\d+"})
+	  */
+	public function changePermissionAction(Conversation $conv, ConversationPermission $perm, $var) {
+                $char = $this->get('dispatcher')->gateway('conversationChangeTest', false, true, false, $conv);
+                if (! $char instanceof Character) {
+                        return $this->redirectToRoute($char);
+                }
+
+		if ($me = $conv->findActiveCharPermission($char)) {
+			$em = $this->getDoctrine()->getManager();
+			$flush = false;
+			$change = 'permission.invalidrequest';
+			$now = new \DateTime("now");
+			echo 'Owner :'.$me->getOwner().'  Manager.: '.$me->getManager().' var cast as '.gettype($var);
+			if ($me->getOwner()) {
+				if (!$perm->getManager()) {
+					if ($var == 0) {
+						$perm->setActive(false);
+						$perm->setEndTime($now);
+						$change = 'permission.demoted.removed';
+						$flush = true;
+						# Yes, newSystemMessage expects a collection. So we cast the char into an array that is then cast into a collection. It works.
+						$message = $this->get('conversation_manager')->newSystemMessage($conv, 'removal', new ArrayCollection([$perm->getCharacter()]), $char, false);
+
+					} elseif ($var == 1) {
+						$perm->setManager(true);
+						$flush = true;
+						$change = 'permission.promoted.manager';
+					}
+				} elseif ($perm->getOwner()) {
+					if ($var == 0) {
+						$perm->setOwner(false);
+						$flush = true;
+						$change = 'permission.demoted.manager';
+					} elseif ($var === 1) {
+						$change = 'permission.promoted.invalid';
+					}
+				} else {
+					if ($var == 0) {
+						$perm->setManager(false);
+						$flush = true;
+						$change = 'permission.demoted.user';
+					} elseif ($var == 1) {
+						$perm->setOwner(true);
+						$flush = true;
+						$change = 'permission.promoted.owner';
+					}
+				}
+			} elseif ($me->getManager()) {
+				if ($perm->getOwner() || $perm->getManager()) {
+					$change = 'permission.invalidrequest';
+				} else {
+					if ($var === 0) {
+						$perm->setActive(false);
+						$perm->setEndTime($now);
+						$flush = true;
+						$change = 'permission.demoted.removed';
+						$message = $this->get('conversation_manager')->newSystemMessage($conv, 'removal', new ArrayCollection([$perm->getCharacter()]), $char, false);
+					} elseif ($var === 1) {
+						$change = 'permission.nopromoteright';
+					}
+				}
+			}
+			if ($flush) {
+				$em->flush();
+			}
+		}
+
+		$this->addFlash('notice', $this->get('translator')->trans($change, ["%name%"=>$perm->getCharacter()->getName()], 'conversations'));
+
+		return $this->redirectToRoute('maf_conv_participants', ['conv'=>$conv->getId()]);
+	}
+
+	/**
+	  * @Route("/{conv}/leave", name="maf_conv_leave", requirements={"conv"="\d+"})
+	  */
+	public function leaveAction(Conversation $conv) {
+                $char = $this->get('dispatcher')->gateway('conversationLeaveTest', false, true, false, $conv);
+                if (! $char instanceof Character) {
+                        return $this->redirectToRoute($char);
+                }
+
+		if ($perm = $conv->findActiveCharPermission($char)) {
+			$em = $this->getDoctrine()->getManager();
+			$perm->setActive(false);
+			$perm->setEndTime(new \DateTime("now"));
+			$message = $this->get('conversation_manager')->newSystemMessage($conv, 'left', null, $char, false);
+			if ($perm->getOwner()) {
+				$perm->setOwner(false);
+				$perm->setManager(false);
+				$this->get('conversation_manager')->findNewOwner($conv, $char, false);
+			} elseif ($perm->getManager()) {
+				$perm->setManager(false);
+			}
+			$em->flush();
+			$this->addFlash('notice', $this->get('translator')->trans('conversation.left', ["%name%"=>$perm->getConversation()->getTopic()], 'conversations'));
+
+			return new RedirectResponse($this->generateUrl('maf_conv_read', ['conv' => $conv->getId()]).'#'.$message->getId());
+		}
+
+		$this->addFlash('notice', $this->get('translator')->trans('conversation.left', ["%name%"=>$perm->getConversation()->getTopic()], 'conversations'));
+		return new RedirectResponse($this->generateUrl('maf_conv_read', ['conv' => $conv->getId()]));
+
+	}
+
+	/**
+	  * @Route("/{conv}/remove", name="maf_conv_remove", requirements={"conv"="\d+"})
+	  */
+	public function removeAction(Conversation $conv) {
+                $char = $this->get('dispatcher')->gateway('conversationRemoveTest', false, true, false, $conv);
+                if (! $char instanceof Character) {
+                        return $this->redirectToRoute($char);
+                }
+
+		if ($perms = $conv->findCharPermissions($char)) {
+			$em = $this->getDoctrine()->getManager();
+			$wasOwner = false;
+			foreach ($perms as $perm) {
+				if ($perm->getOwner()) {
+					$wasOwner = true;
+				}
+				$em->remove($perm);
+			}
+			if ($wasOwner) {
+				$this->get('conversation_manager')->findNewOwner($conv, $char, false);
+			}
+			$em->flush();
+			$this->get('conversation_manager')->pruneConversation($conv);
+			$this->addFlash('notice', $this->get('translator')->trans('conversation.removed', ["%name%"=>$conv->getTopic()], 'conversations'));
+		} else {
+			$this->addFlash('notice', $this->get('translator')->trans('conversation.badremoved', ["%id%"=>$conv->getId()], 'conversations'));
+		}
+
+		return new RedirectResponse($this->generateUrl('maf_conv_summary'));
+	}
+
+
+	/**
+	  * @Route("/{conv}/reply", name="maf_conv_reply", requirements={"conv"="\d+"})
 	  */
 	public function replyAction(Conversation $conv, Request $request) {
                 $char = $this->get('dispatcher')->gateway('conversationReplyTest', false, true, false, $conv);
