79fd508013d1ec8641fe18e6692806bb2a46e2d6
Andrew <Zanaras@users.noreply.github.com>
2020-09-18 22:29:51 +0100

    Update PlaceManageType.php

--- src/BM2/SiteBundle/Form/PlaceManageType.php
+++ src/BM2/SiteBundle/Form/PlaceManageType.php
@@ -11,10 +11,18 @@ use BM2\SiteBundle\Entity\Place;
 
 class PlaceManageType extends AbstractType {
 
-	public function __construct($description, $isowner, Place $me) {
+	private $description;
+	private $isOwner;
+	private $me;
+	private $realms;
+	private $hostable;
+
+	public function __construct($description, $isowner, Place $me, $realms, $hostable) {
 		$this->description = $description;
 		$this->isOwner = $isowner;
 		$this->me = $me;
+		$this->realms = $realm;
+		$this->hostable = $hostable;
 	}
 
 	public function configureOptions(OptionsResolver $resolver) {
@@ -27,27 +35,29 @@ class PlaceManageType extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$isOwner = $this->isOwner;
 		$me = $this->me;
+		$realms = $this->realms;
+		$type = $place->getType()->getName();
 		$name = $me->getName();
 		$formal = $me->getFormalName();
 		$short = $me->getShortDescription();
 		$description = $this->description;
 		if ($isOwner) {
 			$builder->add('name', 'text', array(
-				'label'=>'names.name', 
-				'required'=>true, 
+				'label'=>'names.name',
+				'required'=>true,
 				'data'=>$name,
 				'attr' => array(
-					'size'=>20, 
+					'size'=>20,
 					'maxlength'=>40,
 					'title'=>'help.new.name'
 				)
 			));
 			$builder->add('formal_name', 'text', array(
-				'label'=>'names.formalname', 
-				'required'=>true, 
+				'label'=>'names.formalname',
+				'required'=>true,
 				'data'=>$formal,
 				'attr' => array(
-					'size'=>40, 
+					'size'=>40,
 					'maxlength'=>160,
 					'title'=>'help.new.formalname'
 				)
@@ -65,6 +75,17 @@ class PlaceManageType extends AbstractType {
 			'data'=>$description,
 			'required'=>true,
 		));
+		if ($type == 'embassy') {
+			$builder->add('hosted_realm', EntityType::class, [
+				'required'=>false,
+				'choices'=> $hostable,
+				'class'=>'BM2SiteBundle:Realm',
+				'choice_label' => 'name',
+				'placeholder'=>'manage.hosted.empty',
+				'label'=>'manage.hosted.name'
+			]);
+		}
+		# TODO: the rest of the options.
 	}
 
 	public function getName() {
