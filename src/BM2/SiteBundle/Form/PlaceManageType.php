f5e99db3037ee4fc7b2d57b4944403b7f03d8ad4
Andrew <Zanaras@users.noreply.github.com>
2020-10-26 16:08:12 -0500

    Update PlaceManageType.php

--- src/BM2/SiteBundle/Form/PlaceManageType.php
+++ src/BM2/SiteBundle/Form/PlaceManageType.php
@@ -12,17 +12,13 @@ use BM2\SiteBundle\Entity\Place;
 class PlaceManageType extends AbstractType {
 
 	private $description;
-	private $isOwner;
+	private $type;
 	private $me;
-	private $realms;
-	private $hostable;
 
-	public function __construct($description, $isowner, Place $me, $realms, $hostable) {
+	public function __construct($description, $type, Place $me) {
 		$this->description = $description;
-		$this->isOwner = $isowner;
+		$this->type = $type;
 		$this->me = $me;
-		$this->realms = $realm;
-		$this->hostable = $hostable;
 	}
 
 	public function configureOptions(OptionsResolver $resolver) {
@@ -35,34 +31,39 @@ class PlaceManageType extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$isOwner = $this->isOwner;
 		$me = $this->me;
-		$realms = $this->realms;
 		$type = $place->getType()->getName();
 		$name = $me->getName();
 		$formal = $me->getFormalName();
 		$short = $me->getShortDescription();
 		$description = $this->description;
-		if ($isOwner) {
-			$builder->add('name', 'text', array(
-				'label'=>'names.name',
-				'required'=>true,
-				'data'=>$name,
-				'attr' => array(
-					'size'=>20,
-					'maxlength'=>40,
-					'title'=>'help.new.name'
-				)
-			));
-			$builder->add('formal_name', 'text', array(
-				'label'=>'names.formalname',
-				'required'=>true,
-				'data'=>$formal,
-				'attr' => array(
-					'size'=>40,
-					'maxlength'=>160,
-					'title'=>'help.new.formalname'
-				)
-			));
+
+		if ($place->getSettlement()) {
+			$settlement = $place->getSettlement();
+		} else {
+			$settlement = $place->getGeoFeature()->getGeoData()->getSettlement();
 		}
+
+		$builder->add('name', 'text', array(
+			'label'=>'names.name',
+			'required'=>true,
+			'data'=>$name,
+			'attr' => array(
+				'size'=>20,
+				'maxlength'=>40,
+				'title'=>'help.new.name'
+			)
+		));
+		$builder->add('formal_name', 'text', array(
+			'label'=>'names.formalname',
+			'required'=>true,
+			'data'=>$formal,
+			'attr' => array(
+				'size'=>40,
+				'maxlength'=>160,
+				'title'=>'help.new.formalname'
+			)
+		));
+
 		$builder->add('short_description', 'textarea', array(
 			'label'=>'description.short',
 			'data'=>$short,
@@ -76,16 +77,63 @@ class PlaceManageType extends AbstractType {
 			'required'=>true,
 		));
 		if ($type == 'embassy') {
-			$builder->add('hosted_realm', EntityType::class, [
-				'required'=>false,
-				'choices'=> $hostable,
-				'class'=>'BM2SiteBundle:Realm',
-				'choice_label' => 'name',
-				'placeholder'=>'manage.hosted.empty',
-				'label'=>'manage.hosted.name'
+			$builder->add('for_realm', HiddenType::class, [
+				'data'=>false
 			]);
+			$builder->add('allow_spawn', HiddenType::class, [
+				'data'=>false
+			]);
+			if ($me->getOwner()) {
+				$builder->add('realm', EntityType::class, [
+					'required'=>false,
+					'choices'=> $hosting,
+					'class'=>'BM2SiteBundle:Realm',
+					'choice_label' => 'name',
+					'placeholder'=>'manage.realm.empty',
+					'label'=>'manage.realm.name'
+				]);
+			}
+			if (!$me->getHostingRealm()) {
+				$builder->add('hosting_realm', EntityType::class, [
+					'required'=>false,
+					'choices'=> $place->getRealm()->findHierarchy(true),
+					'class'=>'BM2SiteBundle:Realm',
+					'choice_label' => 'name',
+					'placeholder'=>'manage.hosting.empty',
+					'label'=>'manage.hosting.name'
+				]);
+				$builder->add('owning_realm', HiddenType::class, [
+					'data'=>false
+				]);
+				$builder->add('ambassador', HiddenType::class, [
+					'data'=>false
+				]);
+			} elseif (!$me->getOwningRealm()) {
+				$builder->add('owning_realm', EntityType::class, [
+					'required'=>false,
+					'choices'=> $place->getHostingRealm()->findFriendlyRelations(),
+					'class'=>'BM2SiteBundle:Realm',
+					'choice_label' => 'name',
+					'placeholder'=>'manage.hosted.empty',
+					'label'=>'manage.hosted.name'
+				]);
+				$builder->add('hosting_realm', HiddenType::class, [
+					'data'=>$place->getHostingRealm()
+				]);
+			} else {
+				$builder->add('ambassador', EntityType::class, [
+					'required'=>false,
+					'choices'=>$place->getOwningRealm()->findActiveMembers(),
+					'class'=>'BM2SiteBundle:Character',
+					'choice_label' => 'name',
+					'placeholder'=>'manage.ambassador.empty',
+					'label'=>'manage.ambassador.name'
+				])
+			}
 		}
-		# TODO: the rest of the options.
+		if ($type == 'capital') {
+
+		}
 	}
 
 	public function getName() {
