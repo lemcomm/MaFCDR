7bf34e6693efa6c8ad4b309bd5f66901961b0ef7
Andrew <Zanaras@users.noreply.github.com>
2021-05-18 20:36:16 -0600

    Update DefaultController.php

--- src/BM2/SiteBundle/Controller/DefaultController.php
+++ src/BM2/SiteBundle/Controller/DefaultController.php
@@ -14,15 +14,15 @@ class DefaultController extends Controller {
 
 	/**
 	  * @Route("/", name="bm2_homepage")
-	  * @Template
 	  */
 	public function indexAction() {
-		return array("simple"=>true);
+		return $this->render('Default/index.html.twig', [
+			"simple"=>true
+		]);
 	}
 
 	/**
 	  * @Route("/about", name="bm2_about")
-	  * @Template
 	  */
 	public function aboutAction() {
 		$pr = $this->get('pagereader');
@@ -33,7 +33,7 @@ class DefaultController extends Controller {
 		$gameplay = $pr->getPage('about', 'gameplay', $locale);
 		$tech = $pr->getPage('about', 'technology', $locale);
 
-		return array(
+		return $this->render('Default/about.html.twig', [
 			"simple"=>true,
 			'intro' => $intro,
 			'concept' => $concept,
@@ -41,72 +41,78 @@ class DefaultController extends Controller {
 			'tech' => $tech,
 			'levels' => $this->get('payment_manager')->getPaymentLevels(),
 			'concepturl' => $this->generateUrl('bm2_site_default_paymentconcept'),
-		);
+		]);
 	}
 
 	/**
 	  * @Route("/manual/{page}", name="bm2_manual", defaults={"page"="intro"})
-	  * @Template
 	  */
 	public function manualAction($page) {
 		$toc = $this->get('pagereader')->getPage('manual', 'toc', $this->getRequest()->getLocale());
 		$pagecontent = $this->get('pagereader')->getPage('manual', $page, $this->getRequest()->getLocale());
-		return array(
+
+		return $this->render('Default/manual.html.twig', [
 			"page" => $page,
 			"toc" => $toc,
 			"content" => $pagecontent
-		);
+		]);
 	}
 
 	/**
 	  * @Route("/vips", name="bm2_vips")
-	  * @Template
 	  */
 	public function vipsAction() {
 		$em = $this->getDoctrine()->getManager();
 		$query = $em->createQuery('SELECT u.display_name, u.vip_status FROM BM2SiteBundle:User u WHERE u.vip_status > 0 ORDER BY u.vip_status DESC, u.display_name');
 		$vips = $query->getResult();
 
-		return array("simple"=>true, "vips"=>$vips);
+		return $this->render('Default/vips.html.twig', [
+			"simple"=>true, "vips"=>$vips
+		]);
 	}
 
 
 	/**
      * @Route("/contact", name="bm2_contact")
-     * @Template
      */
 	public function contactAction() {
-		return array("simple"=>true);
+
+		return $this->render('Default/contact.html.twig', [
+			"simple"=>true
+		]);
 	}
 
 	/**
      * @Route("/credits", name="bm2_credits")
-     * @Template
      */
 	public function creditsAction() {
-		return array("simple"=>true);
+
+		return $this->render('Default/credits.html.twig', [
+			"simple"=>true
+		]);
 	}
 
 	/**
      * @Route("/terms", name="bm2_terms")
-     * @Template
      */
 	public function termsAction() {
-		return array("simple"=>true, "locale"=>$this->getRequest()->getLocale());
+
+		return $this->render('Default/terms.html.twig', [
+			"simple"=>true, "locale"=>$this->getRequest()->getLocale()
+		]);
 	}
 
 	/**
      * @Route("/paymentconcept")
-     * @Template
      */
 	public function paymentConceptAction() {
 		$pagecontent = $this->get('pagereader')->getPage('about', 'payment', $this->getRequest()->getLocale());
 
-		return array(
+		return $this->render('Default/terms.html.twig', [
 			"simple"=>true,
 			"content"=>$pagecontent,
 			"paylevels"=>$this->get('payment_manager')->getPaymentLevels()
-		);
+		]);
 	}
 
 
