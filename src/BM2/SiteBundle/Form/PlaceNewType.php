eb2836a5ae60993977fe032f311be05a16699949
Andrew <Zanaras@users.noreply.github.com>
2021-08-14 13:02:57 -0500

    Update PlaceNewType.php

--- src/BM2/SiteBundle/Form/PlaceNewType.php
+++ src/BM2/SiteBundle/Form/PlaceNewType.php
@@ -67,7 +67,7 @@ class PlaceNewType extends AbstractType {
 		));
 		$builder->add('realm', 'entity', array(
 			'label'=>'realm.label',
-			'required'=>true,
+			'required'=>false,
 			'placeholder' => 'realm.empty',
 			'attr' => array('title'=>'help.new.realm'),
 			'class' => 'BM2SiteBundle:Realm',
