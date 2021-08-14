91a1013483cad13ea0382ebaf8506171c8d4caaf
Andrew <Zanaras@users.noreply.github.com>
2021-08-14 18:51:39 -0500

    Update Dispatcher.php

--- src/BM2/SiteBundle/Service/Dispatcher.php
+++ src/BM2/SiteBundle/Service/Dispatcher.php
@@ -3054,16 +3054,17 @@ class Dispatcher {
 		if (($check = $this->politicsActionsGenericTests()) !== true) {
 			return array("name"=>"house.new.name", "description"=>"unavailable.$check");
 		}
-		if ($this->getCharacter()->getHouse()) {
+		$character = $this->getInsidePlace();
+		if ($character->getHouse()) {
 			return array("name"=>"house.new.name", "description"=>"unavailable.havehouse");
 		}
-		if (!$this->getCharacter()->getInsideSettlement() AND !$this->getCharacter()->getInsidePlace()) {
+		if (!$character->getInsideSettlement() AND !$character->getInsidePlace()) {
 			return array("name"=>"house.new.name", "description"=>"unavailable.outsideall");
 		}
-		if ($this->getCharacter()->getInsidePlace() && $this->getCharacter()->getInsidePlace()->getType()->getName() != "home") {
+		if ($character->getInsidePlace() && $character->getInsidePlace()->getType()->getName() != "home") {
 			return array("name"=>"house.new.name", "description"=>"unavailable.wrongplacetype");
 		}
-		if ($character->getInsidePlace() && $character->getInsidePlace()->getOwner() != $this->getCharacter()) {
+		if ($character->getInsidePlace() && $character->getInsidePlace()->getOwner() !== $character) {
 			#TODO: Rework this for permissions when we add House permissions (if we do).
 			return array("name"=>"house.manage.relocate.name", "description"=>"unavailable.notyours2");
 		}
