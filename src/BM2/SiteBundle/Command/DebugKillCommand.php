db6cf6e28ae43dd0a2488571160ed827682aa635
Andrew <Zanaras@users.noreply.github.com>
2021-09-19 13:56:12 -0500

    Create DebugKillCommand.php

---
+++ src/BM2/SiteBundle/Command/DebugKillCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace BM2\SiteBundle\Command;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+class DebugKillCommand extends ContainerAwareCommand {
+
+	protected function configure() {
+		$this
+			->setName('maf:debug:kill')
+			->setDescription('Debug command for fixing failed deaths (by rerunning them)')
+			->addArgument('c', InputArgument::REQUIRED, 'Which character are we killing? Character::id.')
+			->addArgument('k', InputArgument::OPTIONAL, 'Who killed them? Character::id. Can be null.')
+			->addArgument('f', InputArgument::OPTIONAL, 'Was it forced? Boolean. Can be null.')
+			->addArgument('m', InputArgument::OPTIONAL, 'Which message should we use for events? Text.')
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$em = $this->getContainer()->get('doctrine')->getManager();
+                $stopwatch = new Stopwatch();
+		$id = $input->getArgument('c');
+                $output->writeln("Looking for Character #".$id);
+		$char = $em->getRepository('BM2SiteBundle:Character')->findOneById($id);
+		$killer = $input->getArgument('k');
+                $output->writeln("Looking for Killer #".$killer);
+		if ($killer && $killer == 'null') {
+			$killer = null;
+		} elseif ($killer) {
+			$killer = $em->getRepository('BM2SiteBundle:Character')->findOneById($killer);
+		}
+		$forcekiller = $input->getArgument('f');
+		if ($forcekiller && $forcekiller == 'null') {
+			$forcekiller = null;
+		}
+		$msg = $input->getArgument('m');
+
+		$cm = $this->getContainer()->get('character_manager');
+
+		if ($cm->kill($char, $killer, $forcekiller, $msg)) {
+	                $output->writeln('Character '.$char->getName().' ('.$id.') killed succesfully!');
+		} else {
+                	$output->writeln("Something went wrong");
+		}
+
+	}
+}
