b9fb7315d48161bc501178fe4e69c2e6074ac878
Andrew <Zanaras@users.noreply.github.com>
2021-10-25 20:36:53 -0500

    Update AssocCreationType.php

--- src/BM2/SiteBundle/Form/AssocCreationType.php
+++ src/BM2/SiteBundle/Form/AssocCreationType.php
@@ -11,11 +11,9 @@ use BM2\SiteBundle\Entity\AssociationType;
 class AssocCreationType extends AbstractType {
 
 	private $types;
-	private $realms;
 
-	public function __construct($types, $realms) {
+	public function __construct($types) {
 		$this->types = $types;
-		$this->realms = $realms;
 	}
 
 	public function configureOptions(OptionsResolver $resolver) {
@@ -27,14 +25,13 @@ class AssocCreationType extends AbstractType {
 
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$types = $this->types;
-		$realms = $this->realms;
 		$builder->add('name', 'text', array(
 			'label'=>'assoc.form.new.name',
 			'required'=>true,
 			'attr' => array(
 				'size'=>20,
 				'maxlength'=>40,
-				'title'=>'assoc.help.new.name'
+				'title'=>'assoc.help.name'
 			)
 		));
 		$builder->add('formal_name', 'text', array(
@@ -43,14 +40,14 @@ class AssocCreationType extends AbstractType {
 			'attr' => array(
 				'size'=>40,
 				'maxlength'=>160,
-				'title'=>'assoc.help.new.formalname'
+				'title'=>'assoc.help.formalname'
 			)
 		));
 		$builder->add('type', 'entity', array(
 			'label'=>'assoc.form.new.type',
 			'required'=>true,
-			'placeholder' => 'type.empty',
-			'attr' => array('title'=>'help.new.type'),
+			'placeholder' => 'assoc.form.select',
+			'attr' => array('title'=>'assoc.help.type'),
 			'class' => 'BM2SiteBundle:AssociationType',
 			'choice_translation_domain' => true,
 			'choice_label' => 'name',
@@ -59,11 +56,10 @@ class AssocCreationType extends AbstractType {
 		$builder->add('motto', 'text', array(
 			'label'=>'assoc.form.new.motto',
 			'required'=>false,
-			'data'=>$motto,
 			'attr' => array(
 				'size'=>40,
 				'maxlength'=>160,
-				'title'=>'assoc.help.new.motto')
+				'title'=>'assoc.help.motto')
 		));
 		$builder->add('founder', 'text', array(
 			'label'=>'assoc.form.new.founder',
@@ -71,13 +67,13 @@ class AssocCreationType extends AbstractType {
 			'attr' => array(
 				'size'=>20,
 				'maxlength'=>160,
-				'title'=>'assoc.help.new.founder'
+				'title'=>'assoc.help.founder'
 			)
 		));
 		$builder->add('public', 'checkbox', array(
-			'label'=>'assoc.form.description.public',
+			'label'=>'assoc.form.new.public',
 			'attr' => array('title'=>'assoc.help.public'),
-			'placeholder' => true
+			'data' => true
 		));
 		$builder->add('short_description', 'textarea', array(
 			'label'=>'assoc.form.description.short',
@@ -89,9 +85,10 @@ class AssocCreationType extends AbstractType {
 			'attr' => array('title'=>'assoc.help.longdesc'),
 			'required'=>true,
 		));
+		$builder->add('submit', 'submit', array('label'=>'assoc.form.submit'));
 	}
 
 	public function getName() {
-		return 'placenew';
+		return 'assoccreate';
 	}
 }
