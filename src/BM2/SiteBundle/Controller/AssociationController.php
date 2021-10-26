a04e48799b2651095e6bf618fd39c7cca8dc0ecc
Andrew <Zanaras@users.noreply.github.com>
2021-10-25 20:32:47 -0500

    Update AssociationController.php

--- src/BM2/SiteBundle/Controller/AssociationController.php
+++ src/BM2/SiteBundle/Controller/AssociationController.php
@@ -30,10 +30,10 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class AssociationController extends Controller {
 
-	private function dispatcher($test, $assoc = null) {
-		$char = $this->get('dispatcher')->gateway($test, $assoc);
-		if (! $character instanceof Character) {
-			return $this->redirectToRoute($character);
+	private function gateway($test, $secondary = null) {
+		$char = $this->get('dispatcher')->gateway($test, $secondary);
+		if (! $char instanceof Character) {
+			return $this->redirectToRoute($char);
 		}
 		return $char;
 	}
@@ -43,24 +43,18 @@ class AssociationController extends Controller {
 	  */
 
 	public function viewAction(Association $id) {
-		$this->addFlash('notice', "This isn't ready yet, come back later you silly person!");
-		return $this->redirectToRoute('bm2_homepage');
 		$assoc = $id;
 		$details = false;
-		$head = false;
+		$owner = false;
 		$public = false;
 		$char = $this->get('appstate')->getCharacter(false, true, true);
 		if ($char instanceof Character) {
-			foreach ($char->getAssociationMembership() as $member) {
-				if ($member->getAssocation() === $assoc) {
-					$details = true;
-					$public = true;
-					if ($member->getRank()->isHead()) {
-						$head = true;
-					}
-					break;
+			if ($member = $this->get('association_manager')->findMember($id, $char)) {
+				$details = true;
+				$public = true;
+				if ($member->getRank()->getOwner()) {
+					$owner = true;
 				}
-
 			}
 		}
 		if (!$public && $assoc->getPublic()) {
@@ -71,7 +65,7 @@ class AssociationController extends Controller {
 			'assoc' => $assoc,
 			'public' => $public,
 			'details' => $details,
-			'head' => $head
+			'owner' => $owner
 		]);
 	}
 
@@ -80,25 +74,76 @@ class AssociationController extends Controller {
 	  */
 
 	public function createAction(Request $request) {
-		$this->addFlash('notice', "This isn't ready yet, come back later you silly person!");
-		return $this->redirectToRoute('bm2_homepage');
-
 		$char = $this->gateway('assocCreateTest');
 
 		$em = $this->getDoctrine()->getManager();
-		$form = $this->createForm(new AssocCreationType());
+		$form = $this->createForm(new AssocCreationType($em->getRepository('BM2SiteBundle:AssociationType')->findAll()));
 		$form->handleRequest($request);
-		if ($form->isValid()) {
+		if ($form->isValid() && $form->isSubmitted()) {
 			$data = $form->getData();
 
-			$place = $character->getInsidePlace();
-			$settlement = $character->getInsideSettlement();
-			$assoc = $this->get('association_manager')->create($data, null, $place, $settlement, $character);
+			$place = $char->getInsidePlace();
+			$settlement = $char->getInsideSettlement();
+			$assoc = $this->get('association_manager')->create($data, $place, $char);
 			# No flush needed, AssocMan flushes.
-			$this->addFlash('notice', $this->get('translator')->trans('assoc.route.new.created', array(), 'orgs'));
-			return $this->redirectToRoute('maf_house', array('id'=>$house->getId()));
+			$this->addFlash('notice', $this->get('translator')->trans('assoc.route.new.created', [], 'orgs'));
+			return $this->redirectToRoute('maf_assoc', array('id'=>$assoc->getId()));
 		}
 		return $this->render('Assoc/create.html.twig', [
+			'form' => $form->createView()
+		]);
+	}
+
+	/**
+	  * @Route("{id}/createrank", name="maf_assoc_createrank", requirements={"id"="\d+"})
+	  */
+
+	public function createRankAction(Association $id, Request $request) {
+		$assoc = $id;
+		$char = $this->gateway('assocManageRankTest', $assoc);
+		$assocman = $this->get('association_manager');
+		$member = $assocman->findMember($char);
+		$ranks = $member->getRank()->findAllKnownSubordinates();
+
+		$form = $this->createForm(new AssocCreateRankType($ranks));
+		$form->handleRequest($request);
+		if ($form->isValid() && $form->isSubmitted()) {
+			$data = $form->getData();
+
+			$assocman->createRank($assoc, $data['name'], $data['viewAll'], $data['viewUp'], $data['viewDown'], $data['superior'], $data['createSubs'], $data['manager']);
+			# No flush needed, AssocMan flushes.
+			$this->addFlash('notice', $this->get('translator')->trans('assoc.route.rank.created', array(), 'orgs'));
+			return $this->redirectToRoute('maf_assoc_manage', array('id'=>$assoc->getId()));
+		}
+		return $this->render('Assoc/createRank.html.twig', [
+			'form' => $form->createView()
+		]);
+	}
+
+	/**
+	  * @Route("{id}/managerank/{rank}", name="maf_assoc_managerank", requirements={"id"="\d+", "rank"="\d+"})
+	  */
+
+	public function manageRankAction(Association $id, AssociationRank $rank, Request $request) {
+		$assoc = $id;
+		$char = $this->gateway('assocManageRankTest', [$assoc, $rank]);
+
+		$em = $this->getDoctrine()->getManager();
+		$assocman = $this->get('association_manager');
+		$member = $assocman->findMember($char);
+		$ranks = $member->getRank()->findAllKnownSubordinates();
+
+		$form = $this->createForm(new AssocManageRankType($ranks));
+		$form->handleRequest($request);
+		if ($form->isValid() && $form->isSubmitted()) {
+			$data = $form->getData();
+
+			$assocman->updateRank($rank, $data['name'], $data['viewAll'], $data['viewUp'], $data['viewDown'], $data['superior'], $data['createSubs'], $data['manager']);
+			# No flush needed, AssocMan flushes.
+			$this->addFlash('notice', $this->get('translator')->trans('assoc.route.rank.updated', array(), 'orgs'));
+			return $this->redirectToRoute('maf_assoc_manage', array('id'=>$assoc->getId()));
+		}
+		return $this->render('Assoc/manageRank.html.twig', [
 			'form' => $form->createView()
 		]);
 	}
