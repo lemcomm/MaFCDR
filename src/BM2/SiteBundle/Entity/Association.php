e9cd27807888348fd673b2749fa44f996d3eb0ea
Andrew <Zanaras@users.noreply.github.com>
2022-03-19 22:06:42 -0500

    Update Association.php

--- src/BM2/SiteBundle/Entity/Association.php
+++ src/BM2/SiteBundle/Entity/Association.php
@@ -3,36 +3,12 @@
 namespace BM2\SiteBundle\Entity;
 
 use BM2\SiteBundle\Entity\Character;
+use BM2\SiteBundle\Entity\Faction;
 use Doctrine\ORM\Mapping as ORM;
 
 use Doctrine\Common\Collections\ArrayCollection;
 
-/**
- * Association
- */
-class Association {
-
-	protected $ultimate=false;
-
-	public function findUltimate() {
-		if ($this->ultimate!==false) {
-			return $this->ultimate;
-		}
-		if (!$superior=$this->getSuperior()) {
-			$this->ultimate=$this;
-		} else {
-			while ($superior->getSuperior()) {
-				$superior=$superior->getSuperior();
-			}
-			$this->ultimate=$superior;
-		}
-		return $this->ultimate;
-	}
-
-	public function isUltimate() {
-		if ($this->findUltimate() == $this) return true;
-		return false;
-	}
+class Association extends Faction {
 
         public function findAllMemberCharacters($include_myself=true) {
                 $all_chars = new ArrayCollection;
@@ -56,22 +32,18 @@ class Association {
                 return $all_members;
         }
 
-        public function findAllInferiors($include_myself = false) {
-                $all_infs = new ArrayCollection;
-                if ($include_myself) {
-                        $all_infs->add($this);
-                }
-                foreach ($this->getInferiors() as $inf) {
-                        $all_infs->add($inf);
-                        $suball = $inf->findAllInferiors();
-                        foreach ($suball as $sub) {
-                                if (!$all->contains($sub)) {
-                                        $all->add($sub);
-                                }
+	public function findActiveMembers($with_subs = true, $forceupdate = false) {
+                $all_members = new ArrayCollection;
+                $all_infs = $this->findAllInferiors(true);
+                foreach ($all_infs as $inf) {
+                        foreach ($inf->getMembers() as $infMember) {
+				if ($infMember->isActive()) {
+                                	$all_members->add($infMember);
+				}
                         }
                 }
-                return $all_infs;
-        }
+                return $all_members;
+	}
 
 	public function findMember(Character $char, $all = false) {
 		if ($all) {
@@ -85,29 +57,6 @@ class Association {
 			}
 		}
 		return false;
-	}
-
-	public function findLaw($search) {
-		foreach ($this->getLaws() as $law) {
-			if ($law->getType()->getName() == $search) {
-				return $law;
-			}
-		}
-		return false;
-	}
-
-	public function findMutlipleLaws($haystack) {
-		$needles = [];
-		foreach ($this->getLaws() as $law) {
-			if (in_array($law->getType()->getName(), $haystack)) {
-				$needles[] = $law;
-			}
-		}
-		if (is_empty($needles)) {
-			return false;
-		} else {
-			return $needles;
-		}
 	}
 
 	public function isPublic() {
@@ -127,6 +76,5 @@ class Association {
 		}
 		return $all;
 	}
-  
-  
+	
 }
