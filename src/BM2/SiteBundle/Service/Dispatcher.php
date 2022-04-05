3d69515ecade28c9f4d9368fbdfe72728829e480
Andrew <Zanaras@users.noreply.github.com>
2022-04-05 17:58:49 -0500

    Update Dispatcher.php

--- src/BM2/SiteBundle/Service/Dispatcher.php
+++ src/BM2/SiteBundle/Service/Dispatcher.php
@@ -2399,8 +2399,10 @@ class Dispatcher {
 		}
 		if ($character->getInsideSettlement()) {
 			$can = $this->permission_manager->checkSettlementPermission($character->getInsideSettlement(), $character, 'placeinside');
+		} elseif ($region = $this->geography->findMyRegion($character)) {
+			$can = $this->permission_manager->checkSettlementPermission($region->getSettlement(), $character, 'placeoutside');
 		} else {
-			$can = $this->permission_manager->checkSettlementPermission($this->geography->findMyRegion($character)->getSettlement(), $character, 'placeoutside');
+			return array("name"=>"place.new.name", "description"=>"unavailable.nosettlement");
 		}
 		if ($can) {
 			# It's a long line, but basically, but if we're in a settlement or in a region and have the respective permission, we're allowed. If not, denied.
