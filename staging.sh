9bd07588f4c7ea13bf80b5983f959919efdc4020
Andrew <Zanaras@users.noreply.github.com>
2022-04-06 21:39:12 -0500

    Update staging.sh

--- staging.sh
+++ staging.sh
@@ -4,10 +4,8 @@ sudo /etc/init.d/apache2 stop
 sudo -u www-data /bin/rm -rf app/cache/dev/* app/cache/test/* app/cache/prod/*
 /bin/rm -rf app/cache/dev/* app/cache/test/* app/cache/prod/*
 php ~/composer.phar dump-autoload --optimize
-#php app/console cache:clear --env=test --no-warmup
-php app/console cache:clear --env=prod --no-warmup
+php app/console cache:clear --env=prod
 php app/console doctrine:generate:entities BM2 --no-backup
-php app/console doctrine:generate:entities Calitarus --no-backup
 php app/console assets:install
 #php app/console cache:warmup --env=test
 php app/console cache:warmup --env=prod
@@ -24,5 +22,5 @@ sudo /etc/init.d/apache2 start
 
 # Fix the fucking permissions that never stick:
 echo "fixing permissions"
-sudo /usr/bin/setfacl -dR -m u:www-data:rwX -m u:maf:rwX /home/maf/symfony/app/cache /home/maf/symfony/app/logs /home/maf/symfony/app/spool
-sudo /usr/bin/setfacl -R -m u:www-data:rwX -m u:maf:rwX /home/maf/symfony/app/cache /home/maf/symfony/app/logs /home/maf/symfony/app/spool
+sudo /usr/bin/setfacl -dR -m u:www-data:rwX -m u:maf:rwX /var/www/maf/app/cache /var/www/maf/app/logs
+sudo /usr/bin/setfacl -R -m u:www-data:rwX -m u:maf:rwX /var/www/maf/app/cache /var/www/maf/app/logs
