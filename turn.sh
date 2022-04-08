9017b94ead4e35579fa11342b4dcd38ca50def64
Andrew <Zanaras@users.noreply.github.com>
2022-04-07 21:08:20 -0500

    Update turn.sh

--- turn.sh
+++ turn.sh
@@ -17,6 +17,7 @@ php $APP maf:process:expires --env=prod 2>&1 > $LOGDIR/turn-$DAY.log
 php $APP maf:run -t -d turn --env=prod 2>&1 >> $LOGDIR/turn-$DAY.log
 php $APP maf:process:economy --env=prod -t 2>&1 >> $LOGDIR/turn-$DAY.log
 php $APP maf:process:convs --env=prod 2>&1 >> $LOGDIR/turn-$DAY.log
+php $APP maf:process:slumbers -t --env=prod 2>&1 >> $LOGDIR/turn-$DAY.log
 echo "----- turn done -----" >> $LOGDIR/turn-$DAY.log
 
 # If you're on a server where you want mails, you may want to setup the below as appropriate.
@@ -26,15 +27,15 @@ echo "----- turn done -----" >> $LOGDIR/turn-$DAY.log
 php $APP maf:stats:turn --env=prod -d 2>&1 > $LOGDIR/stats.log
 
 # map generation and legacy map storage
- cp /var/www/qgis/maps/allrealms.png /var/www/qgis/maps/history/allrealms-$DATE.png
- cp /var/www/qgis/maps/2ndrealms.png /var/www/qgis/maps/history/2ndrealms-$DATE.png
- cp /var/www/qgis/maps/majorrealms.png /var/www/qgis/maps/history/majorrealms-$DATE.png
- curl -so /var/www/qgis/maps/allrealms.png "http://maps.mightandfealty.com/qgis?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=0,0,512000,512000&CRS=EPSG:4326&WIDTH=2048&HEIGHT=2048&LAYERS=water,blocked,AllRealms&FORMAT=image/png&map=MapWithRealms.qgs"
- curl -so /var/www/qgis/maps/2ndrealms.png "http://maps.mightandfealty.com/qgis?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=0,0,512000,512000&CRS=EPSG:4326&WIDTH=2048&HEIGHT=2048&LAYERS=water,blocked,2ndLevelRealms&FORMAT=image/png&map=MapWithRealms.qgs"
- curl -so /var/www/qgis/maps/majorrealms.png "http://maps.mightandfealty.com/qgis?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=0,0,512000,512000&CRS=EPSG:4326&WIDTH=2048&HEIGHT=2048&LAYERS=water,blocked,MajorRealms&FORMAT=image/png&map=MapWithRealms.qgs"
- convert /var/www/qgis/maps/allrealms.png -resize 256x256 /var/www/qgis/maps/allrealms-thumb.png
- convert /var/www/qgis/maps/2ndrealms.png -resize 256x256 /var/www/qgis/maps/2ndrealms-thumb.png
- convert /var/www/qgis/maps/majorrealms.png -resize 256x256 /var/www/qgis/maps/majorrealms-thumb.png
+# cp /var/www/qgis/maps/allrealms.png /var/www/qgis/maps/history/allrealms-$DATE.png
+# cp /var/www/qgis/maps/2ndrealms.png /var/www/qgis/maps/history/2ndrealms-$DATE.png
+# cp /var/www/qgis/maps/majorrealms.png /var/www/qgis/maps/history/majorrealms-$DATE.png
+# curl -so /var/www/qgis/maps/allrealms.png "http://maps.mightandfealty.com/qgis?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=0,0,512000,512000&CRS=EPSG:4326&WIDTH=2048&HEIGHT=2048&LAYERS=water,blocked,AllRealms&FORMAT=image/png&map=MapWithRealms.qgs"
+# curl -so /var/www/qgis/maps/2ndrealms.png "http://maps.mightandfealty.com/qgis?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=0,0,512000,512000&CRS=EPSG:4326&WIDTH=2048&HEIGHT=2048&LAYERS=water,blocked,2ndLevelRealms&FORMAT=image/png&map=MapWithRealms.qgs"
+# curl -so /var/www/qgis/maps/majorrealms.png "http://maps.mightandfealty.com/qgis?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=0,0,512000,512000&CRS=EPSG:4326&WIDTH=2048&HEIGHT=2048&LAYERS=water,blocked,MajorRealms&FORMAT=image/png&map=MapWithRealms.qgs"
+# convert /var/www/qgis/maps/allrealms.png -resize 256x256 /var/www/qgis/maps/allrealms-thumb.png
+# convert /var/www/qgis/maps/2ndrealms.png -resize 256x256 /var/www/qgis/maps/2ndrealms-thumb.png
+# convert /var/www/qgis/maps/majorrealms.png -resize 256x256 /var/www/qgis/maps/majorrealms-thumb.png
 
 # For backup purposes, it may be super handy to download them backups! Or upload them, as is the case below.
 # scp $BACKUPDIR/maf-$DAY.sql.gz your.host.org:~/backups/
