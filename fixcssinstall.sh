4cab2d1c3b03bde29cc2529fdc40e37eb24169ca
Andrew <Zanaras@users.noreply.github.com>
2022-05-05 17:57:39 -0500

    Create fixcssinstall.sh

---
+++ fixcssinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "making folder"
+mkdir /var/www/maf/web/bundles/bm2site/css
+echo "moving css files"
+mv /var/www/maf/web/bundles/bm2site/*.css /var/www/maf/web/bundles/bm2site/css
