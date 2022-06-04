27ca70c04f62a637928b8b13d4945748b869ddb4
Andrew <Zanaras@users.noreply.github.com>
2022-06-03 21:27:33 -0500

    Update UserDataType.php

--- src/BM2/SiteBundle/Form/UserDataType.php
+++ src/BM2/SiteBundle/Form/UserDataType.php
@@ -3,6 +3,9 @@
 namespace BM2\SiteBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -10,9 +13,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class UserDataType extends AbstractType {
 
 	private $gm;
+	private $text;
+	private $admin;
 
-	public function __construct($gm) {
+	public function __construct($gm, $text, $admin) {
 		$this->gm = $gm;
+		$this->text = $text;
+		$this->admin = $admin;
 	}
 
 	public function configureOptions(OptionsResolver $resolver) {
@@ -25,14 +32,36 @@ class UserDataType extends AbstractType {
 
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$builder->add('username', null, array('label' => 'form.username', 'attr' => array('title' => 'registration.help.username', 'class'=>'tt_bot')));
-		$builder->add('email', 'email', array('label' => 'form.email', 'attr' => array('title' => 'registration.help.email', 'class'=>'tt_bot')));
+		$builder->add('email', EmailType::class, array('label' => 'form.email', 'attr' => array('title' => 'registration.help.email', 'class'=>'tt_bot')));
 
 		$builder->add('display_name', null, array('label' => 'form.displayname', 'attr' => array('title' => 'form.help.displayname', 'class'=>'tt_bot')));
 		if ($this->gm) {
 			$builder->add('gm_name', null, array('label' => 'form.gmname', 'attr' => array('title' => 'form.help.gmname', 'class'=>'tt_bot')));
 		} else {
-			$builder->add('gm_name', 'hidden', array('data' => null));
+			$builder->add('gm_name', HiddenType::class, array('data' => null, 'required'=>false));
 		}
+		if ($this->admin) {
+			$builder->add('public_admin', null, array('label' => 'form.publicadmin', 'attr' => array('title' => 'form.help.publicadmin', 'class'=>'tt_bot')));
+		} else {
+			$builder->add('public_admin', HiddenType::class, array('data' => null, 'required'=>false));
+		}
+		$builder->add('public', null, [
+			'label' => 'form.public',
+			'attr' => [
+				'title'=>'form.help.public',
+				'class'=>'tt_bot'
+			]
+		]);
+		$builder->add('text', TextareaType::class, [
+			'label'=>'form.profile',
+			'data'=>$this->text,
+			'mapped'=>false,
+			'required'=>true,
+			'attr' => [
+				'title'=>'form.help.profile',
+				'class'=>'tt_bot'
+			]
+		]);
 
 		$builder->add('submit', 'submit', array('label'=>'form.submit'));
 	}
