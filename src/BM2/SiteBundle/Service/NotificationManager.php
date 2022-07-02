a23386fabf93182445f5b3d1fa26fa1d29dfcb6a
Andrew <Zanaras@users.noreply.github.com>
2022-07-02 03:04:17 -0500

    Update NotificationManager.php

--- src/BM2/SiteBundle/Service/NotificationManager.php
+++ src/BM2/SiteBundle/Service/NotificationManager.php
@@ -4,6 +4,7 @@ namespace BM2\SiteBundle\Service;
 
 use BM2\SiteBundle\Entity\Artifact;
 use BM2\SiteBundle\Entity\Association;
+use BM2\SiteBundle\Entity\BattleReport;
 use BM2\SiteBundle\Entity\Character;
 use BM2\SiteBundle\Entity\House;
 use BM2\SiteBundle\Entity\Event;
@@ -18,14 +19,18 @@ class NotificationManager {
 	protected $appstate;
 	protected $mailman;
 	protected $msgtrans;
+	protected $trans;
+	protected $discord;
 	private $type;
 	private $name;
 
-	public function __construct(EntityManager $em, AppState $appstate, MailManager $mailman, MessageTranslateExtension $msgtrans) {
+	public function __construct(EntityManager $em, AppState $appstate, MailManager $mailman, MessageTranslateExtension $msgtrans, $translator, DiscordIntegrator $discord) {
 		$this->em = $em;
 		$this->appstate = $appstate;
 		$this->mailman = $mailman;
 		$this->msgtrans = $msgtrans;
+		$this->trans = $translator;
+		$this->discord = $discord;
 	}
 
 	private function findUser(Event $event) {
@@ -91,6 +96,52 @@ class NotificationManager {
 			#TODO: Expand this if we ever use other notification types. Like push notifications, or something to an app, etc.
 			$this->mailman->spoolEvent($event, $user, $msg);
 		}
+	}
+
+	public function spoolBattle(BattleReport $rep, $epic) {
+		$em = $this->em;
+		$entity = false;
+		if ($loc = $rep->getLocationName()) {
+			if ($rep->getPlace()) {
+				$entity = $em->getRepository("BM2SiteBundle:Place")->find($loc['id']);
+				$url = 'https://mightandfealty.com/place/'.$loc['id'];
+			} else {
+				$entity = $em->getRepository("BM2SiteBundle:Settlement")->find($loc['id']);
+				$url = 'https://mightandfealty.com/settlement/'.$loc['id'];
+			}
+		}
+		if (!$entity) {
+			return;
+		}
+		if ($loc['key'] === 'battle.location.nowhere') {
+			$str = 'in lands unknown(!?)';
+		} elseif ($loc['key'] === 'battle.location.of') {
+			$str = 'at ['.$entity->getName().']('.$url.')';
+		} elseif ($loc['key'] === 'battle.location.siege') {
+			$str = 'during the siege of ['.$entity->getName().']('.$url.')';
+		} elseif ($loc['key'] === 'battle.location.sortie') {
+			$str = 'started by the defenders of ['.$entity->getName().']('.$url.')';
+		} elseif ($loc['key'] === 'battle.location.assault') {
+			$str = 'during the assault of ['.$entity->getName().']('.$url.')';
+		} elseif ($loc['key'] === 'battle.location.near') {
+			$str = 'near ['.$entity->getName().']('.$url.')';
+		} elseif ($loc['key'] === 'battle.location.around') {
+			$str = 'in the vicinity of ['.$entity->getName().']('.$url.')';
+		} elseif ($loc['key'] === 'battle.location.castle') {
+			$str = 'in the halls of ['.$entity->getName().']('.$url.')';
+		}
+		if ($epic > 9) {
+			$txt = "Tales are spun and epics created about a legendary battle ".$str."!";
+		} elseif ($epic > 6) {
+			$txt = "Tales are spun and epics created about a massive battle ".$str."!";
+		} elseif ($epic > 4) {
+			$txt = "Tales are spun and epics created about a huge battle ".$str."!";
+		} elseif ($epic > 3) {
+			$txt = "Tales are spun and epics created about a large battle ".$str."!";
+		} else {
+			$txt = "Tales are spun and epics created about a battle ".$str."!";
+		}
+		$this->discord->pushToGeneral($txt);
 	}
 
 }
