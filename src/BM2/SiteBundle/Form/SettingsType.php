fbd6e7716f241997c9b2bb3de56e2ba34bb5d69d
Andrew <Zanaras@users.noreply.github.com>
2022-07-01 23:29:49 -0500

    Update SettingsType.php

--- src/BM2/SiteBundle/Form/SettingsType.php
+++ src/BM2/SiteBundle/Form/SettingsType.php
@@ -3,6 +3,10 @@
 namespace BM2\SiteBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -26,17 +30,37 @@ class SettingsType extends AbstractType {
 
 	// FIXME: change this to use the user object (it's very old code, I didn't know about it)
 	public function buildForm(FormBuilderInterface $builder, array $options) {
-		$builder->add('notifications', 'checkbox', array(
-			'label' => 'account.settings.notifications',
-			'required' => false,			
-			'data'=>$this->user->getNotifications()
-		));
-		$builder->add('newsletter', 'checkbox', array(
+		$builder->add('newsletter', CheckboxType::class, array(
 			'label' => 'account.settings.newsletter',
-			'required' => false,			
+			'required' => false,
 			'data'=>$this->user->getNewsletter()
 		));
-		$builder->add('language', 'choice', array(
+		$builder->add('notifications', CheckboxType::class, array(
+			'label' => 'account.settings.notifications',
+			'required' => false,
+			'data'=>$this->user->getNotifications()
+		));
+		$builder->add('emailDelay', ChoiceType::class, array(
+			'label' => 'account.settings.delay.name',
+			'required' => false,
+			'placeholder' => 'account.settings.delay.choose',
+			'choices'=>[
+				'now' => 'account.settings.delay.now',
+				'hourly' => 'account.settings.delay.hourly',
+				'6h' => 'account.settings.delay.6h',
+				'12h' => 'account.settings.delay.12h',
+				'daily' => 'account.settings.delay.daily',
+				'sundays' => 'account.settings.delay.sundays',
+				'mondays' => 'account.settings.delay.mondays',
+				'tuesdays' => 'account.settings.delay.tuesdays',
+				'wednesdays' => 'account.settings.delay.wednesdays',
+				'thursdays' => 'account.settings.delay.thursdays',
+				'fridays' => 'account.settings.delay.fridays',
+				'saturdays' => 'account.settings.delay.saturdays',
+			],
+			'data'=>$this->user->getEmailDelay()
+		));
+		$builder->add('language', ChoiceType::class, array(
 			'label' => 'account.settings.language',
 			'placeholder' => 'form.browser',
 			'required' => false,
@@ -44,7 +68,7 @@ class SettingsType extends AbstractType {
 			'data'=>$this->user->getLanguage()
 		));
 
-		$builder->add('submit', 'submit', array('label'=>'account.settings.submit'));
+		$builder->add('submit', SubmitType::class, array('label'=>'account.settings.submit'));
 	}
 
 	public function getName() {
