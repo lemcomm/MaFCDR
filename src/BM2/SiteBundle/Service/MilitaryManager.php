0e4f0e468510593b679c9835a60f6eae05dfde1f
Andrew <Zanaras@users.noreply.github.com>
2022-07-26 19:54:12 -0500

    Update MilitaryManager.php

--- src/BM2/SiteBundle/Service/MilitaryManager.php
+++ src/BM2/SiteBundle/Service/MilitaryManager.php
@@ -90,7 +90,7 @@ class MilitaryManager {
 		switch($this->getClassName($entity)) {
 			case 'Settlement':
 				if ($with_trainers) {
-					$query = $this->em->createQuery('SELECT e as item, ba.resupply FROM BM2SiteBundle:EquipmentType e LEFT JOIN e.provider p LEFT JOIN p.buildings ba LEFT JOIN ba.settlement sa LEFT JOIN e.trainer t LEFT JOIN t.buildings bb LEFT JOIN bb.settlement sb WHERE sa = :location AND ba.active = true AND sb = :location AND bb.active = true ORDER BY t.name ASC, e.name ASC');
+					$query = $this->em->createQuery('SELECT e as item, ba.resupply FROM BM2SiteBundle:EquipmentType e LEFT JOIN e.provider p LEFT JOIN p.buildings ba LEFT JOIN ba.settlement sa LEFT JOIN e.trainer t LEFT JOIN t.buildings bb LEFT JOIN bb.settlement sb WHERE sa = :location AND ba.active = true AND sb = :location AND bb.active = true ORDER BY p.name ASC, e.name ASC');
 				} else {
 					$query = $this->em->createQuery('SELECT e as item, b.resupply FROM BM2SiteBundle:EquipmentType e LEFT JOIN e.provider p LEFT JOIN p.buildings b LEFT JOIN b.settlement s WHERE s = :location AND b.active = true ORDER BY p.name ASC, e.name ASC');
 				}
