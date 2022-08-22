e373b5c824acc0d4ada5cf23c39148b513500f49
Andrew <Zanaras@users.noreply.github.com>
2022-08-21 19:35:07 -0500

    Update Activity.php

--- src/BM2/SiteBundle/Entity/Activity.php
+++ src/BM2/SiteBundle/Entity/Activity.php
@@ -2,6 +2,7 @@
 
 namespace BM2\SiteBundle\Entity;
 
+use BM2\SiteBundle\Entity\Character;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -13,6 +14,27 @@ class Activity {
                 foreach ($this->participants as $p) {
                         if ($p === $this->organizer) {
                                 return $p;
+                        }
+                }
+                return false;
+        }
+
+        public function findChallenged() {
+                foreach ($this->participants as $p) {
+                        if ($p !== $this->organizer) {
+                                return $p;
+                        }
+                }
+                return false;
+        }
+
+        public function isAnswerable(Character $char) {
+                foreach ($this->participants as $p) {
+                        if ($p->getCharacter() === $char && $p->isChallenged() && !$p->getAccepted()) {
+                                return true;
+                        }
+                        if ($p->getCharacter() === $char && $p->isChallenger() && $p->getActivity()->findChallenged()->getAccepted() && !$p->getAccepted()) {
+                                return true;
                         }
                 }
                 return false;
