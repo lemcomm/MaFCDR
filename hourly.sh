5e2219324787584e465e7ed9806b3fdef4d96de7
Andrew <Zanaras@users.noreply.github.com>
2022-12-18 12:00:48 -0600

    Update hourly.sh

--- hourly.sh
+++ hourly.sh
@@ -10,7 +10,7 @@ APP="/var/www/maf/app/console"
 # sudo setfacl -dR -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
 # sudo setfacl -R -m u:www-data:rwX -m u:maf:rwX ~/symfony/app/cache ~/symfony/app/logs ~/symfony/app/spool
 
-php $APP --env=prod maf:process:activities -t 2>&1 >> $LOGDIR/hourly.log
+php $APP --env=prod maf:process:activities 2>&1 >> $LOGDIR/hourly.log
 php $APP --env=prod maf:process:familiarity -t 2>&1 >> $LOGDIR/hourly.log
 php $APP --env=prod maf:process:travel -t 2>&1 >> $LOGDIR/hourly.log
 php $APP --env=prod maf:process:spotting -t 2>&1 >> $LOGDIR/hourly.log
