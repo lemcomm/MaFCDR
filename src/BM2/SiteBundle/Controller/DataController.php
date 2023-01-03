854c68c8fc041eee00a7e062627aa2f3e613dadd
Andrew <Zanaras@users.noreply.github.com>
2023-01-03 02:02:26 -0600

    Update DataController.php

--- src/BM2/SiteBundle/Controller/DataController.php
+++ src/BM2/SiteBundle/Controller/DataController.php
@@ -34,7 +34,7 @@ class DataController extends Controller {
 		$cycle = $this->get('appstate')->getCycle()-1;
 		$query = $em->createQuery('SELECT s.today_users as active_users FROM BM2SiteBundle:StatisticGlobal s WHERE s.cycle = :cycle');
 		$query->setParameter('cycle', $cycle);
-		$result['active_players'] = $query->getArrayResult()[0];
+		$result['active_players'] = $query->getArrayResult()[0]['active_users'];
 		$result['name'] = "Might & Fealty";
 		$result['image_url'] = 'https://mightandfealty.com/bundles/bm2site/images/logo-transparent.png';
 		$result['description'] = 'An entirely player driven medieval sandbox game about politics and war set in a low-ish-fantasy world.';
