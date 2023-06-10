02e74a8a1a8b5ccd61ec2147cf5f048cd4d19379
Andrew <Zanaras@users.noreply.github.com>
2023-06-10 13:54:19 -0500

    Update turn.sh

--- turn.sh
+++ turn.sh
@@ -13,6 +13,7 @@ pg_dump -C maf | gzip > $BACKUPDIR/maf-$DAY.sql.gz
 # sudo setfacl -R -m u:www-data:rwX -m u:maf:rwX /home/maf/symfony/app/cache /home/maf/symfony/app/logs /home/maf/symfony/app/spool
 # sudo setfacl -dR -m u:www-data:rwX -m u:maf:rwX /home/maf/symfony/app/cache /home/maf/symfony/app/logs /home/maf/symfony/app/spool
 
+php $APP maf:tor:update 2>&1 > $LOGDIR/exits-$DAY.log
 php $APP maf:process:expires --env=prod 2>&1 > $LOGDIR/turn-$DAY.log
 php $APP maf:process:economy --env=prod -t 2>&1 >> $LOGDIR/turn-$DAY.log
 php $APP maf:run -t -d turn --env=prod 2>&1 >> $LOGDIR/turn-$DAY.log
