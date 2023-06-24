67d612a547d49669d9bf7adb0119f282883fee06
Andrew <Zanaras@users.noreply.github.com>
2023-06-24 18:50:55 -0500

    Update GameRunner.php

--- src/BM2/SiteBundle/Service/GameRunner.php
+++ src/BM2/SiteBundle/Service/GameRunner.php
@@ -5,6 +5,7 @@ namespace BM2\SiteBundle\Service;
 use BM2\SiteBundle\Entity\Character;
 use BM2\SiteBundle\Entity\Election;
 use BM2\SiteBundle\Entity\RealmPosition;
+use BM2\SiteBundle\Entity\Siege;
 use BM2\SiteBundle\Entity\Supply;
 use CrEOF\Spatial\PHP\Types\Geometry\Point;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -36,6 +37,7 @@ class GameRunner {
 	private $pm;
 	private $npc;
 	private $cm;
+	private $wm;
 
 	private $cycle=0;
 	private $output=false;
@@ -45,7 +47,7 @@ class GameRunner {
 	private $bandits_ok_distance = 50000;
 	private $seen;
 
-	public function __construct(EntityManager $em, AppState $appstate, Logger $logger, ActionResolution $resolver, Economy $economy, Politics $politics, History $history, MilitaryManager $milman, BattleRunner $battlerunner, Interactions $interactions, Geography $geography, Generator $generator, RealmManager $rm, ConversationManager $convman, PermissionManager $pm, NpcManager $npc, CharacterManager $cm) {
+	public function __construct(EntityManager $em, AppState $appstate, Logger $logger, ActionResolution $resolver, Economy $economy, Politics $politics, History $history, MilitaryManager $milman, BattleRunner $battlerunner, Interactions $interactions, Geography $geography, Generator $generator, RealmManager $rm, ConversationManager $convman, PermissionManager $pm, NpcManager $npc, CharacterManager $cm, WarManager $wm) {
 		$this->em = $em;
 		$this->appstate = $appstate;
 		$this->logger = $logger;
@@ -63,6 +65,7 @@ class GameRunner {
 		$this->pm = $pm;
 		$this->npc = $npc;
 		$this->cm = $cm;
+		$this->wm = $wm;
 
 		$this->cycle = $this->appstate->getCycle();
 		$this->speedmod = (float)$this->appstate->getGlobal('travel.speedmod', 1.0);
@@ -479,7 +482,7 @@ class GameRunner {
 		return true;
 	}
 
-	public function runSoldierUpdatesCycle() {
+	public function runSoldierCycle() {
 		$last = $this->appstate->getGlobal('cycle.soldiers', 0);
 		if ($last==='complete') return true;
 		$date = date("Y-m-d H:i:s");
@@ -1298,6 +1301,44 @@ class GameRunner {
 		$this->em->clear();
 
 		return true;
+	}
+
+	public function updateSieges() {
+		$this->logger->info("Sieges cleanup..");
+		$all = $this->em->getRepository(Siege::class)->findAll();
+		foreach ($all as $siege) {
+			$settlement = $siege->getSettlement();
+			$place = $siege->getPlace();
+			$attacker = $siege->getAttacker();
+			if ($attacker->getLeader()) {
+				$leader = $attacker->getLeader();
+				if ($settlement && ($leader->getInsideSettlement() == $settlement || $leader->getInsidePlace()->getSettlement() == $settlement)) {
+					# Attacking leader is somehow inside the settlement he is besieging, looks like siege should be over! :D
+					$this->logger->info("  Disbanding Siege ".$siege->getId()." for Settlement ".$settlement->getId());
+					foreach ($siege->getCharacters() as $char) {
+						$this->history->logEvent(
+							$char,
+							'siege.leaderinside.settlement',
+							['%link-character%'=>$leader->getId(), '%link-settlement%'=>$settlement->getId()]
+						);
+					}
+					$this->wm->disbandSiege($siege, $leader, true);
+
+				}
+				if ($place && $leader->getInsidePlace() === $place) {
+					# Attacking leader is somehow inside the place he is besieging, looks like siege should be over! :D
+					$this->logger->info("  Disbanding Siege ".$siege->getId()." for Place ".$place->getId());
+					foreach ($siege->getCharacters() as $char) {
+						$this->history->logEvent(
+							$char,
+							'siege.leaderinside.place',
+							['%link-character%'=>$leader->getId(), '%link-place%'=>$place->getId()]
+						);
+					}
+					$this->wm->disbandSiege($siege, $leader, true);
+				}
+			}
+		}
 	}
 
 	public function runConversationsCleanup() {
