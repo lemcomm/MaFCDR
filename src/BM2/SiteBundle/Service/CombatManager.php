4673863eb8687719e7ec57b871b358a4bfc56d8c
Andrew <Zanaras@users.noreply.github.com>
2023-08-25 21:19:27 -0500

    2.8.3.4 (#243)
    * Unit size does not affect combat power (#240)
    
    * Unit size does not affect combat power
    
    * Update CombatManager.php
    
    Add $groupSize, which will always be passed by BattleRunner, to indicate the full fighting soldier count of that side of battle.
    
    ---------
    
    Co-authored-by: Andrew <Zanaras@users.noreply.github.com>
    
    * Update BattleRunner.php
    
    * Paramour text fix (#242)
    
    * Update messages.en.yml
    
    Added prebattle on request, made things look more exciting!
    
    * Update politics.en.yml
    
    * Royal Mews can be built in scrublands. (#241)
    
    * Royal Mews can be built in scrublands.
    
    * Manual updated to reflect the changes
    
    ---------
    
    Co-authored-by: Rohesie <rohesie@gmail.com>
    Co-authored-by: Demivar <32566786+Demivar@users.noreply.github.com>

--- src/BM2/SiteBundle/Service/CombatManager.php
+++ src/BM2/SiteBundle/Service/CombatManager.php
@@ -299,7 +299,7 @@ class CombatManager {
 		return [$result, $logs];
 	}
 
-	public function MeleePower($me, $sol = false, EquipmentType $weapon = null) {
+	public function MeleePower($me, $sol = false, EquipmentType $weapon = null, $groupSize = 1) {
 		$noble = false;
 		$act = false;
 		# $sol is just a bypass for "Is this a soldier instance" or not.
@@ -374,9 +374,8 @@ class CombatManager {
 
 		// TODO: heavy armour should reduce this a little
 		if ($sol) {
-			$fighters = $me->getAllInUnit()->count();
-			if ($fighters>1) {
-				$me->updateMeleePower($power * pow($fighters, 0.96)/$fighters);
+			if ($groupSize>1) {
+				$me->updateMeleePower($power * pow($groupSize, 0.96)/$groupSize);
 			} else {
 				$me->updateMeleePower($power);
 			}
@@ -450,7 +449,7 @@ class CombatManager {
 		return [$result, $logs];
 	}
 
-	public function RangedPower($me, $sol = false, EquipmentType $weapon = null) {
+	public function RangedPower($me, $sol = false, EquipmentType $weapon = null, $groupSize = 1) {
 		$noble = false;
 		# $sol is just a bypass for "Is this a soldier instance" or not.
 		if ($sol) {
@@ -527,9 +526,8 @@ class CombatManager {
 		// TODO: heavy armour should reduce this quite a bit
 
 		if ($sol) {
-			$fighters = $me->getAllInUnit()->count();
-			if ($fighters>1) {
-				$me->updateRangedPower($power * pow($fighters, 0.96)/$fighters);
+			if ($groupSize>1) {
+				$me->updateRangedPower($power * pow($groupSize, 0.96)/$groupSize);
 			} else {
 				$me->updateRangedPower($power);
 			}
