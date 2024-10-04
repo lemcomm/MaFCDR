46e5185299a9d805772dbc71095e09dd1e5c4f29
Andrew <Zanaras@users.noreply.github.com>
2024-10-03 19:59:02 -0500

    2.9.3.10

--- src/BM2/SiteBundle/Service/Dispatcher.php
+++ src/BM2/SiteBundle/Service/Dispatcher.php
@@ -692,7 +692,6 @@ class Dispatcher {
 		$actions[] = $this->diplomacyHierarchyTest();
 		$actions[] = $this->diplomacySubrealmTest();
 		$actions[] = $this->diplomacyBreakHierarchyTest();
-		$actions[] = $this->diplomacyRestoreTest();
 
 		return array("name"=>"diplomacy", "elements"=>$actions);
 	}
